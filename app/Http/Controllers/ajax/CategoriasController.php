411d7fa584c72f542e916b50d07d436e4331beaf
vagrant <vagrant@homestead>
2018-03-01 06:20:00 +0000

    Agrega clases de modelos y controller faltantes

---
+++ app/Http/Controllers/ajax/CategoriasController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\ajax;
+
+use App\CategoriaActividad;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CategoriasController extends Controller
+{
+    public function show(Request $request)
+    {
+        return CategoriaActividad::find($request->id)->tipos();
+    }
+}
---
+++ resources/assets/js/components/filtro.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="row">
+        <div class="col-md-3">
+            <select name="actividad">
+                <option value="1">Actividades en Asentamientos</option>
+                <option value="2">Eventos Especiales</option>
+                <option value="3">Actividades en Oficina</option>
+            </select>
+        </div>
+        <div class="col-md-3">
+            <select name="tipo">
+
+                    <option value="@{{ tipo.idTipo }}">@{{ tipo.nombre}}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="col-md-3">
+            <select name="zona">
+                <option value="1">Buenos Aires</option>
+                <option value="2">Capital Federal</option>
+                <option value="3">Otro</option>
+            </select>
+        </div>
+        <div class="col-md-3">
+            <button class="btn btn-primary btn-large">Borra Filtros</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "filtro",
+        props: ['tipos']
+    }
+</script>
