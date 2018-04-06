339cc4ecc13731709282f11148b30868a2977da8
Alejandro Abraham <alejandro.abe88@gmail.com>
2018-04-06 18:27:08 -0300

    TEC-83 Refactor de busqueda de actividades

---
+++ app/Search/TiposActividadesSearch.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Search;
+
+use App\Actividad;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+
+class TiposActividadesSearch
+{
+    public static function apply(Request $filters)
+    {
+        $query = static::applyDecoratorsFromRequest($filters, TiposActividadesSearch::newQuery());
+        return static::getResults($query);
+    }
+    private static function applyDecoratorsFromRequest(Request $request, Builder $query)
+    {
+        foreach ($request->all() as $filterName => $value) {
+            $decorator = static::createFilterDecorator($filterName);
+            if (static::isValidDecorator($decorator)) {
+                $query = $decorator::apply($query, $value);
+            }
+        }
+        return $query;
+    }
+    private static function createFilterDecorator($name)
+    {
+        return __NAMESPACE__ . '\\Filters\\' . studly_case($name);
+    }
+    private static function isValidDecorator($decorator)
+    {
+        return class_exists($decorator);
+    }
+    private static function getResults(Builder $query)
+    {
+        return $query->get();
+    }
+
+    private static function newQuery(){
+        $query = (new Actividad)->newQuery();
+        $query->join('Tipo', 'Actividad.idTipo', '=', 'Tipo.idTipo')
+            ->leftJoin('PuntoEncuentro', 'Actividad.idActividad', '=', 'PuntoEncuentro.idActividad')
+            ->orderBy('Tipo.nombre', 'asc')
+            ->selectRaw('distinct Tipo.idTipo, Tipo.nombre');
+        return $query;
+    }
+}
