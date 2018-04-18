1cf8e11b2266a03e2c882ee909a65a4802505c5f
Alejandro Balbi <Balbi>
2018-04-18 16:55:35 -0300

    [TEC-99] Hacer template de email de recordatorio de actividad

---
+++ resources/views/emails/recordatorioActividad.blade.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>Recordatorio Actividad</title>
+</head>
+ 
+<body>
+<h2>{{$persona->nombres}}</h2>
+<h2>Te inscribiste a {{$actividad->nombreActividad}} que tendra lugar el {{$actividad->fechaInicio}}</h2>
+<br/>
+</body>
+ 
+</html>
