f2fea383f8e99dd4bc2d6523a7024601199bd708
Alejandro Balbi <Balbi>
2018-04-24 16:10:03 -0300

    [TEC-97] Implementa layout para mail de confirmacion en inscripcion

--- resources/views/emails/confimacionInscripcion.blade.php
+++ resources/views/emails/confimacionInscripcion.blade.php
@@ -4,92 +4,51 @@
 </head>
  
 <body>
-<h2>Te inscribiste a {{$inscripcion->actividad->nombreActividad}}</h2>
-<br/>
-<table cellpadding="0" cellspacing="0" border="0" width="100%" style="background-color:#e7f4f3">
-  <tbody>
-    <tr>
-      <td style="background-color:#e7f4f3">
-        <table  align="center" style="box-sizing:border-box;border-spacing:0;border-collapse:collapse;padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;width:100%;max-width:600px;margin-top:0;margin-bottom:0;margin-right:auto;margin-left:auto;background-color:#e7f4f3" bgcolor="#e7f4f3">
-          <tbody>
-            <tr style="padding-top:0;padding-bottom:0;padding-right:0;padding-left:0">
-              <td  style="border-collapse:collapse!important;word-break:break-word;font-family:'Helvetica Neue',Helvetica,Arial,sans-serif;font-weight:400;line-height:15.6px;margin-top:0;margin-bottom:0;margin-right:0;margin-left:0;color:#333333;font-size:0;padding-top:5px;padding-bottom:5px;padding-right:10px;padding-left:10px;text-align:center">
-                <table width="100%" style="border-spacing:0;border-collapse:collapse;font-family:'Helvetica Neue',Helvetica,Arial,sans-serif;font-weight:400;line-height:15.6px;margin-top:0;margin-bottom:0;margin-right:0;margin-left:0;padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;color:#333333">
-                  <tbody>
-                    <tr style="padding-top:0;padding-bottom:0;padding-right:0;padding-left:0">
-                      <td style="border-collapse:collapse!important;word-break:break-word;font-family:'Helvetica Neue',Helvetica,Arial,sans-serif;font-weight:400;line-height:15.6px;margin-top:0;margin-bottom:0;margin-right:0;margin-left:0;color:#333333;padding-top:5px;padding-bottom:5px;padding-right:10px;padding-left:10px">
-                        <table  style="border-spacing:0;border-collapse:collapse;font-family:'Helvetica Neue',Helvetica,Arial,sans-serif;font-weight:400;line-height:15.6px;margin-top:0;margin-bottom:0;margin-right:0;margin-left:0;padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;color:#333333;width:100%">
-                          <tbody>
-                            <tr style="padding-top:0;padding-bottom:0;padding-right:0;padding-left:0">
-                              <td align="center" style="padding:0;line-height:0px;border-collapse:collapse!important;word-break:break-word;margin-top:0;margin-bottom:0;margin-right:0;margin-left:0">
-                                <a href="http://lt.dplrtrack.info/d1fc34c0c8c685cc41a1ac022ef18a34-1c48a2db5ffee7dd9b1dd7531c928471" target="_blank" data-saferedirecturl="https://www.google.com/url?hl=en&amp;q=http://lt.dplrtrack.info/d1fc34c0c8c685cc41a1ac022ef18a34-1c48a2db5ffee7dd9b1dd7531c928471&amp;source=gmail&amp;ust=1524658654821000&amp;usg=AFQjCNGUZWZ0zVC7sIJhXV486cTo8ykbfA">
-                                  <img width="170" src="https://ci3.googleusercontent.com/proxy/I832R7MPMTddg8uqPYosZcK-e15_wnOiiVkpRDDvj2L9JkXAuIhgy779gwds6MGO8L4MXOYRFQUHIsEa3DvbpeP5KS02fzHCkuZ1HuoEcpHwazw6Id_Deiei-PbA1-t1cEBJqAzC9vbAosIRCAo=s0-d-e1-ft#http://app2.dopplerfiles.com/Users/120713/Campaigns/9630476/Color_RGB_-_Sin_fondo(1).png" alt="Color_RGB_-_Sin_fondo(1)" style="clear:both;width:170px;max-width:100%;text-decoration:none;border-style:none;outline-style:none;text-align:center" class="CToWUd">
-                                </a>
-                              </td>
-                            </tr>
-                          </tbody>
-                        </table>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </td>
-    </tr>
-  </tbody>
-</table>
-<table align="center" style="box-sizing:border-box;border-spacing:0;border-collapse:collapse;padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;width:100%;max-width:600px;margin-top:0;margin-bottom:0;margin-right:auto;margin-left:auto;background-color:#e7f4f3" bgcolor="#e7f4f3">
-  <tbody>
-    <tr style="padding-top:0;padding-bottom:0;padding-right:0;padding-left:0">
-      <td style="border-collapse:collapse!important;word-break:break-word;font-family:'Helvetica Neue',Helvetica,Arial,sans-serif;font-weight:400;line-height:15.6px;margin-top:0;margin-bottom:0;margin-right:0;margin-left:0;color:#333333;font-size:0;padding-top:5px;padding-bottom:5px;padding-right:10px;padding-left:10px;text-align:center">            
-        <table width="100%" style="border-spacing:0;border-collapse:collapse;font-family:'Helvetica Neue',Helvetica,Arial,sans-serif;font-weight:400;line-height:15.6px;margin-top:0;margin-bottom:0;margin-right:0;margin-left:0;padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;color:#333333">              
-          <tbody>
-            <tr style="padding-top:0;padding-bottom:0;padding-right:0;padding-left:0">
-              <td style="border-collapse:collapse!important;word-break:break-word;font-family:'Helvetica Neue',Helvetica,Arial,sans-serif;font-weight:400;line-height:15.6px;margin-top:0;margin-bottom:0;margin-right:0;margin-left:0;color:#333333;padding-top:2.5px;padding-bottom:5px;padding-right:10px;padding-left:10px">
-                <table  style="border-spacing:0;border-collapse:collapse;font-family:'Helvetica Neue',Helvetica,Arial,sans-serif;font-weight:400;line-height:15.6px;margin-top:0;margin-bottom:0;margin-right:0;margin-left:0;padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;color:#333333;width:100%">                    
-                  <tbody>
-                    <tr style="vertical-align:top">
-                      <td style="word-break:break-word;border-collapse:collapse!important;vertical-align:top;padding:0" align="center">
-                        <table  border="0" cellpadding="0" cellspacing="0" style="text-align:center"> 
-                          <tbody>
-                            <tr>
-                              <td style="display:inline-block;padding-right:5px;padding-top:5px;line-height:0px" valign="middle">
-                                  <a href="http://lt.dplrtrack.info/a1ccecf1d4a82082506189b30dbb3789-1c48a2db5ffee7dd9b1dd7531c928471" target="_blank" data-saferedirecturl="https://www.google.com/url?hl=en&amp;q=http://lt.dplrtrack.info/a1ccecf1d4a82082506189b30dbb3789-1c48a2db5ffee7dd9b1dd7531c928471&amp;source=gmail&amp;ust=1524658654821000&amp;usg=AFQjCNFBy0FCWzlSTZDEAzLTyaMYyZolAA">                                
-                                    <img width="32" src="https://ci6.googleusercontent.com/proxy/4z4sMZMCDRxTvJQ80EWv2pIS6X1DVzR6-HpHoy4s1smjOrrxCm0QuP-DXfTkN6eeng0qTQCPoRE1weKrrgurD6o8vTVnWL_X0LsbzjQx7pqyXmvjajfFkohrDJ4xgA=s0-d-e1-ft#http://app2.dopplerfiles.net/MSEditor/images/hollow_rounded_facebook.png" alt="Facebook" class="CToWUd">
-                                  </a>
-                              </td>
-                              <td style="display:inline-block;padding-right:5px;padding-top:5px;line-height:0px" valign="middle">
-                                <a href="http://lt.dplrtrack.info/47414522b64f1ac0be8d6c95febc6ce9-1c48a2db5ffee7dd9b1dd7531c928471" target="_blank" data-saferedirecturl="https://www.google.com/url?hl=en&amp;q=http://lt.dplrtrack.info/47414522b64f1ac0be8d6c95febc6ce9-1c48a2db5ffee7dd9b1dd7531c928471&amp;source=gmail&amp;ust=1524658654821000&amp;usg=AFQjCNFFaREy-RpR-hkJTBR7pK0AUsYVGw">
-                                  <img width="32" src="https://ci6.googleusercontent.com/proxy/E55JS6uJV_Vo20UYP29bmyyiflaXvrPclD7Gm9JUH66mzwwZ50sX6YTF0HiIdJr7BpY7BxR-hSsW91-DTD8x_qMzY8v4krvQGquI6DvBpdRXUOFA4mKX4_b_Ns4PrwA=s0-d-e1-ft#http://app2.dopplerfiles.net/MSEditor/images/hollow_rounded_instagram.png" alt="Instagram" class="CToWUd">
-                                </a>
-                              </td>
-                              <td style="display:inline-block;padding-right:5px;padding-top:5px;line-height:0px" valign="middle">
-                                <a href="http://lt.dplrtrack.info/b9ae56197721ed27d3ec89b5f615b4d0-1c48a2db5ffee7dd9b1dd7531c928471" target="_blank" data-saferedirecturl="https://www.google.com/url?hl=en&amp;q=http://lt.dplrtrack.info/b9ae56197721ed27d3ec89b5f615b4d0-1c48a2db5ffee7dd9b1dd7531c928471&amp;source=gmail&amp;ust=1524658654821000&amp;usg=AFQjCNG-Sehf0nB3nTxcvr3Ig0FgjukAZQ">
-                                  <img width="32" src="https://ci5.googleusercontent.com/proxy/-ZjxGxqAK1rsZG3Hhaw_HAMD72HY36M-G5sANgV0BlZ7Xw8F-VH5M2g1NZjDTm-Qf76iWtfkI_PyzKMItn1NZrUnREoUqZOu6wr5pnbGr2a-Rto4Inh3Kx9XN0GVIA=s0-d-e1-ft#http://app2.dopplerfiles.net/MSEditor/images/hollow_rounded_linkedin.png" alt="Linkedin" class="CToWUd">
-                                </a>
-                              </td>
-                              <td style="display:inline-block;padding-right:0px;padding-top:5px;line-height:0px" valign="middle">
-                                <a href="http://lt.dplrtrack.info/b6390e349653bb94601a7b7ae8742719-1c48a2db5ffee7dd9b1dd7531c928471" target="_blank" data-saferedirecturl="https://www.google.com/url?hl=en&amp;q=http://lt.dplrtrack.info/b6390e349653bb94601a7b7ae8742719-1c48a2db5ffee7dd9b1dd7531c928471&amp;source=gmail&amp;ust=1524658654821000&amp;usg=AFQjCNFViqL5Gg4nAfzjPLmrGyKSNN8wrA">
-                                  <img width="32" src="https://ci5.googleusercontent.com/proxy/Uf5bIAODa7MzN2AokMwVesevh9AGELXbebXj1MD1D1HMZavmM8hP5JI9PjI8KUftnPqgi3Qy-zeI7zDMPeWwgzVyNZ_X4TFAGh-sk6SgsE3us9R8V2VQMiv5xxo4=s0-d-e1-ft#http://app2.dopplerfiles.net/MSEditor/images/hollow_rounded_twitter.png" alt="Twitter" class="CToWUd">
-                                </a>
-                              </td>
-                            </tr>
-                          </tbody>
-                        </table>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>                
-              </td>              
-            </tr>            
-          </tbody>
-        </table> 
-      </td>       
-    </tr>      
-  </tbody>
-</table>
- 
+  <table border="0" style="border:1px solid #999999;font-family:Helvetica,sans-serif;font-size:12px" align="center">
+    <tbody>
+      <tr>
+        <td>
+          <p>Hola {{$inscripcion->persona->nombres}} {{$inscripcion->persona->apellidoPaterno}}: </p>
+          <p> Te has inscrito para participar en <strong>{{$inscripcion->actividad->nombreActividad}}</strong> de TECHO - {{$inscripcion->actividad->pais->nombre}} que inicia el {{$inscripcion->actividad->fechaInicio->format('d/m/Y')}}.  </p>
+          <b>En:</b> {{$inscripcion->actividad->localidad->localidad}} {{$inscripcion->actividad->provincia->provincia}}
+          <b>Coordinador:</b> {{$inscripcion->actividad->coordinador->nombres}} {{$inscripcion->actividad->coordinador->apellidoPaterno}}
+          <p>{{$inscripcion->actividad->mensajeInscripcion}} <br></p>
+          @if($inscripcion->actividad->LinkPago)
+          <p>
+            <br>
+            <b>
+              <span style="font-size:20px">Ahora SOLO FALTA UN PASO: 
+                <span style="color:rgb(255,153,0)">ABONAR LA CONSTRUCCIÓN </span>
+              </span>
+            </b>
+            <br>
+            <br>
+            <b>TENÉS TIEMPO HASTA EL VIERNES 17 DE NOVIEMBRE</b> para confirmar tu inscripción!<br>
+            <br>
+            Te dejamos el siguiente <a href="{{$inscripcion->actividad->LinkPago}}">BOTÓN DE PAGO</a> e <a href="https://sites.google.com/a/techo.org/veni-a-construir/pago" target="_blank" >INSTRUCTIVO</a> que te permiten gestionar cómo querés abonar la construcción.<br>
+            Te recordamos que el monto para abonar es de <b>${{$inscripcion->actividad->costo}}</b>, los cuales cubren los gastos de traslado, seguro y comida durante la construcción. En el caso que no puedas abonarlo, no queremos que dejes de participar, escribinos a <a href="mailto:problemasdepago.argentina@techo.org" target="_blank">problemasdepago.argentina@<wbr>techo.org</a> para poder gestionar una PRÓRROGA o BECA.<br>
+            <br>
+          </p>
+          @endif
+          <p><b>Te esperamos!!!</b></p>
+          <p>Punto de encuento</p>
+          <ul>
+            <li>
+              <b>{{$inscripcion->punto_encuento->punto}}</b> - {{$inscripcion->punto_encuento->localidad->localidad}} - {{$inscripcion->punto_encuento->provincia->provincia}} - {{$inscripcion->punto_encuento->pais->nombre}} <b>horario:</b> {{$inscripcion->punto_encuento->horario}} <b>coordinador:</b> {{$inscripcion->punto_encuento->responsable->nombres}} {{$inscripcion->punto_encuento->responsable->apellidoPaterno}}
+            </li>
+          </ul>
+          
+          <p> Para TECHO - Argentina es importante que te mantengas enterado de las nuevas actividades. Para ello, entra siempre en nuestro Sitio Web.  </p>
+            <p> Muchas gracias!!  </p>
+          </p>
+        </td> 
+      </tr>
+      <tr>
+        <td> 
+          @include('emails.footer') 
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</body>
 </html>
