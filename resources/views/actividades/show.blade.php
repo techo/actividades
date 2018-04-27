7a562d1670d45dbd08ff431a243b73ecaff828b8
Johan Marchan <johan.marchan@globallogic.com>
2018-04-27 13:14:20 -0300

    [TEC-161] Incluye detalles de la ubicación en Blade de detalle de la actividad

--- resources/views/actividades/show.blade.php
+++ resources/views/actividades/show.blade.php
@@ -1,7 +1,7 @@
 @extends('main')
 
 @section('page_title')
-    Detalle de Actividad
+    {{ $actividad->tipo->nombre }}:  {{ $actividad->nombreActividad }}
 @endsection
 
 
@@ -38,7 +38,13 @@
 		<div class="row justify-content-start">
 			<div class="col-md-2"><i class="far fa-calendar"></i> <span>{{ $actividad->fechaInicio->format('d-m-Y')}}</span></div>
 			<div class="col-md-2"><i class="far fa-clock"></i> <span>{{ $actividad->fechaInicio->format('h:m')}}</span></div>
-			<div class="col-md-2"><i class="fas fa-map-marker-alt"></i> <span>{{ $actividad->localidad->localidad }}</span></div>
+            <div class="col-md-8">
+                <i class="fas fa-map-marker-alt"></i>
+                <span>
+                    {{ $actividad->localidad->localidad }}, {{ $actividad->provincia->provincia }}
+                    , {{ $actividad->pais->nombre }}
+                </span>
+            </div>
 		</div>
 		<hr>
 		<div class="row">
@@ -66,17 +72,6 @@
 		<hr>
 		<div class="row">
 			<div class="col-md-12">
-				<h5>Donde es la actividad</h5>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-12">
-				<p>{{ $actividad->lugar }}</p>
-			</div>
-		</div>
-		<hr>
-		<div class="row">
-			<div class="col-md-12">
 				<h5>Puntos de encuentro</h5>
 			</div>
 		</div>
@@ -98,101 +93,11 @@
                 </div>
 			</div>
 		@endforeach
-		<hr>
-		<div class="row">
-			<div class="col-md-12">
-				<h2>Actividades relacionadas</h2>
-			</div>
-		</div>
-		<div class="card-deck">
-			<div class="card">
-				<img src="https://placeholdit.co/i/555x150?bg=d3d3d3">
-				<div class="row">
-					<div class="col-md-12">
-						<h6>[Tipo actividad]</h6>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-md-12">
-						<h5>[Nombre actividad]</h5>
-					</div>
-				</div>
-				<hr>
-				<div class="row">
-					<div class="col-md-4"><i class="far fa-calendar"></i> <span>[d-m-Y]</span></div>
-					<div class="col-md-4"><i class="far fa-clock"></i> <span>[h:m]</span></div>
-					<div class="col-md-4"><i class="fas fa-map-marker-alt"></i> <span>[lugar]</span></div>
-				</div>
-				<hr>
-				<div class="row">
-					<div class="col-md-12">
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat.
-					</div>
-				</div>
-			</div>
-			<div class="card">
-				<img src="https://placeholdit.co/i/555x150?bg=d3d3d3">
-				<div class="row">
-					<div class="col-md-12">
-						<h6>[Tipo actividad]</h6>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-md-12">
-						<h5>[Nombre actividad]</h5>
-					</div>
-				</div>
-				<hr>
-				<div class="row">
-					<div class="col-md-4"><i class="far fa-calendar"></i> <span>[d-m-Y]</span></div>
-					<div class="col-md-4"><i class="far fa-clock"></i> <span>[h:m]</span></div>
-					<div class="col-md-4"><i class="fas fa-map-marker-alt"></i> <span>[lugar]</span></div>
-				</div>
-				<hr>
-				<div class="row">
-					<div class="col-md-12">
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat.
-					</div>
-				</div>
+            <br>
+            <p class="text-muted text-right">
+                * Tendrás más detalle sobre los puntos de encuentro cuando completes tu inscripción.
+            </p>
 
-			</div>
-			<div class="card">
-				<img src="https://placeholdit.co/i/555x150?bg=d3d3d3">
-				<div class="row">
-					<div class="col-md-12">
-						<h6>[Tipo actividad]</h6>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-md-12">
-						<h5>[Nombre actividad]</h5>
-					</div>
-				</div>
-				<hr>
-				<div class="row">
-					<div class="col-md-4"><i class="far fa-calendar"></i> <span>[d-m-Y]</span></div>
-					<div class="col-md-4"><i class="far fa-clock"></i> <span>[h:m]</span></div>
-					<div class="col-md-4"><i class="fas fa-map-marker-alt"></i> <span>[lugar]</span></div>
-				</div>
-				<hr>
-				<div class="row">
-					<div class="col-md-12">
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat.
-					</div>
-				</div>
-
-			</div>
-
-		</div>
 @endsection
 
 @section('footer')
@@ -206,9 +111,18 @@
 	@if (Auth::check() && Auth::user()->estaInscripto($actividad->idActividad))
 		<div class="col-md-3"><span class="btn btn-success w-100">YA TE INSCRIBISTE!</span></div>
 	@elseif($hayCupos && $inscripcionAbierta)
-		<div class="col-md-3"><a class="btn btn-primary inscripcion-btn w-100" href="/inscripciones/actividad/{{$actividad->idActividad}}">INSCRIBIRME</a></div>
+        <div class="col-md-3">
+            <a class="btn btn-primary inscripcion-btn w-100"
+               href="/inscripciones/actividad/{{$actividad->idActividad}}">
+                INSCRIBIRME
+            </a>
+        </div>
 	@else
-		<div class="col-md-3"><span class="btn btn-danger inscripcion-btn w-100">INSCRIPCIÓN CERRADA</span></div>
+        <div class="col-md-3">
+            <span class="btn btn-danger inscripcion-btn w-100">
+                INSCRIPCIÓN CERRADA
+            </span>
+        </div>
 	@endif
 </footer>
 
