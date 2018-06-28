df552ed88473fb6296099703bcf1930526f72cbb
Johan Marchan <jmarchan@gmail.com>
2018-06-28 11:01:52 -0300

    Incluye botón de evaluación en template

--- resources/views/emails/invitacionEvaluacion.blade.php
+++ resources/views/emails/invitacionEvaluacion.blade.php
@@ -14,4 +14,17 @@
         {{$actividad->localidad->localidad}}, {{$actividad->provincia->provincia}}.
     </p>
 
+    <p>Para darnos tu opinión, haz click en el enlace </p>
+    <p>
+        <a href="{{ url('/actividades/'. $actividad->idActividad .'/evaluaciones') }}">
+            <img src="{{ asset('/img/boton_evaluar.png') }}" alt="Ir a evaluaciones">
+        </a>
+    </p>
+
+    <p>
+        Si no puedes usar el enlace, copia y pega esta dirección en tu navegador:
+        <a href="{{ url('/actividades/'. $actividad->idActividad .'/evaluaciones') }}">
+            {{ url('/actividades/'. $actividad->idActividad .'/evaluaciones') }}
+        </a>
+    </p>
 @endsection
