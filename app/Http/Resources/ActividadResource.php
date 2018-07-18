9522ac159b08d5864407914003461282122a4507
Johan Marchan <jmarchan@gmail.com>
2018-07-18 11:07:57 -0300

    Tarjetas para las actividades futuras, datatable para las pasadas

--- app/Http/Resources/ActividadResource.php
+++ app/Http/Resources/ActividadResource.php
@@ -15,29 +15,26 @@ class ActividadResource extends Resource
     public function toArray($request)
     {
         return [
-            'idActividad' => $this->idActividad,
-            'tipo' => new TipoResource($this->tipo),
-            'fecha' => $this->fechaInicio->format('d/m'),
-            'hora' => $this->fechaInicio->format('H:i'),
-            'fechaInicio' => empty($this->fechaInicio) ? '' : $this->fechaInicio->format('d-m-Y'),
-            'fechaFin' => empty($this->fechaFin) ? '' : $this->fechaFin->format('d-m-Y'),
-            'fechaInicioInscripciones' =>empty($this->fechaInicioInscripciones) ? '' : $this->fechaInicioInscripciones->format('d-m-Y'),
-            'fechaFinInscripciones' => empty($this->fechaFinInscripciones) ? '' : $this->fechaFinInscripciones->format('d-m-Y'),
-            'fechaInicioEvaluaciones' =>empty($this->fechaInicioEvaluaciones) ? '' : $this->fechaInicioEvaluaciones->format('d-m-Y H:i'),
-            'fechaFinEvaluaciones' => empty($this->fechaFinEvaluaciones) ? '' : $this->fechaFinEvaluaciones->format('d-m-Y H:i'),
-            'nombreActividad' => $this->nombreActividad,
-            'descripcion' => $this->descripcion,
-            'compromiso' => $this->compromiso,
-            'costo' => $this->costo,
-            'lugar' => $this->localidad->localidad . ', ' . $this->provincia->provincia,
-            'moneda' => $this->moneda,
-            'puntosEncuentro' => PuntoEncuentroResource::collection($this->puntosEncuentro),
-            'localidad' => $this->localidad,
-            'inscriptos' => $this->idPersonaInscriptos($this->idActividad),
-            'limiteInscripciones' => $this->limiteInscripciones,
+            'idActividad'   => $this->idActividad,
+            'tipo'          => new TipoResource($this->tipo),
+            'fecha'         => $this->fechaInicio->format('d/m'),
+            'hora'          => $this->fechaInicio->format('H:i'),
+            'fechaInicio'   => empty($this->fechaInicio) ? '' : $this->fechaInicio->format('d-m-Y'),
+            'fechaFin'      => empty($this->fechaFin) ? '' : $this->fechaFin->format('d-m-Y'),
+            'fechaInicioInscripciones'  => empty($this->fechaInicioInscripciones) ? '' : $this->fechaInicioInscripciones->format('d-m-Y'),
+            'fechaFinInscripciones'     => empty($this->fechaFinInscripciones) ? '' : $this->fechaFinInscripciones->format('d-m-Y'),
+            'nombreActividad'           => $this->nombreActividad,
+            'descripcion'   => $this->descripcion,
+            'compromiso'    => $this->compromiso,
+            'costo'         => $this->costo,
+            'lugar'         => $this->lugar,
+            'moneda'        => $this->moneda,
+            'puntosEncuentro'           => PuntoEncuentroResource::collection($this->puntosEncuentro),
+            'localidad'     => $this->localidad,
+            'inscriptos'    => $this->idPersonaInscriptos($this->idActividad),
+            'limiteInscripciones'       => $this->limiteInscripciones,
             'cantInscriptos' => $this->inscripciones()->inscripto()->count(),
-            'cuposRestantes' => $this->limiteInscripciones - $this->inscripciones()->inscripto()->count(),
-            'presente' => (isset($this->presente) && $this->presente == 1) ? 1 : 0
+            'cuposRestantes' => (int)$this->limiteInscripciones - $this->inscripciones()->inscripto()->count()
         ];
     }
 
---
+++ resources/assets/js/components/perfil/tarjeta-horizontal.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="row">
+        <div class="col-md-3">
+            <img :src="rowData.img" alt="rowData.tipo" width="120">
+        </div>
+        <div class="col-md-9">
+            <div class="small-caps">
+                {{ rowData.tipo }}
+            </div>
+            <div><a :href="urlDetalle">{{ rowData.nombreActividad }}</a></div>
+
+            <span class="small-text"><i class="fas fa-calendar-alt"></i> {{ rowData.fecha }}</span>
+            <span class="small-text"><i class="fas fa-clock"></i> {{ rowData.hora }}</span>
+            <span class="small-text"><i class="fas fa-map-marker-alt"></i> {{ rowData.lugar }}</span>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "tarjeta-horizontal",
+    props: {
+        rowData: {
+            type: Object,
+            required: true
+        },
+        rowIndex: {
+            type: Number
+        }
+    },
+    data: function () {
+        return {
+            idActividad: '',
+            //urlDetalle: '/actividades/' + this.rowData.idActividad
+        }
+    },
+    watch: {
+        'rowData.idActividad': function(nuevo) {
+            this.idActividad = nuevo;
+        }
+    },
+    computed: {
+        urlDetalle: function () {
+            return '/actividades/' + this.idActividad;
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .small-caps {
+        font-size: smaller;
+        font-variant: all-petite-caps;
+    }
+    .small-text {
+        font-size: smaller;
+    }
+
+    span.small-text {
+        margin-right: 10px;
+    }
+</style>
