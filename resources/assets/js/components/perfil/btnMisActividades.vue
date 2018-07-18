c4553fa32c39ac4fba146ecc3a5403f7e21e5c41
Johan Marchan <jmarchan@gmail.com>
2018-07-18 11:07:57 -0300

    Tarjetas para las actividades futuras, datatable para las pasadas

--- resources/assets/js/components/perfil/btnMisActividades.vue
+++ resources/assets/js/components/perfil/btnMisActividades.vue
@@ -1,10 +1,16 @@
 <template>
-    <div>
+    <div class="contenedor">
         <simplert ref="confirmar"></simplert>
-        <button v-if="!actividadPasada" type="button" class="btn btn-sm btn-warning">Desinscribirse</button>
-        <button v-if="actividadPasada && periodoDeEvaluacionYaComenzo" class="btn btn-sm btn-info">Ver Evaluaciones</button>
-        <span v-if="actividadPasada && !periodoDeEvaluacionYaComenzo" class="small-text">
-            El período de evaluaciones comienza el <br>{{ this.rowData.fechaInicioEvaluaciones }}
+        <button
+                v-if="actividadPasada && periodoDeEvaluacionYaComenzo"
+                class="btn btn-sm btn-info"
+                @click="ir_a_evaluar"
+        >
+            Ver Evaluaciones
+        </button>
+        <span v-if="actividadPasada && !periodoDeEvaluacionYaComenzo" class="small-caps">
+            El período de evaluaciones comienza el <br>
+            {{ this.rowData.fechaInicioEvaluaciones }}
         </span>
     </div>
 </template>
@@ -23,49 +29,19 @@
         },
         data: function() {
             return {
-
+                idActividad: ''
             }
         },
         methods: {
             ir_a_evaluar: function () {
-                window.location.href = '/actividades/' + this.inscripcion.idActividad + '/evaluaciones'
+                window.location.href = '/actividades/' + this.idActividad + '/evaluaciones'
             },
-            ir_a_actividad: function () {
-                window.location.href = '/actividades/' + this.inscripcion.idActividad
-            },
-            desincribir: function (idActividad) {
-                var self = this;
-                self.$refs.confirmar.openSimplert({
-                    title:'DESINSCRIBIRME DE ACTIVIDAD',
-                    message:"Estás por desinscribirte de la actividad " + self.inscripcion.actividad.nombreActividad + ", se borrarán tus datos para participar. Puedes inscribirte cuando desees. ¿Deseas continuar?",
-                    useConfirmBtn: true,
-                    isShown: true,
-                    disableOverlayClick: true,
-                    customClass: 'confirmar',
-                    customCloseBtnText: 'CANCELAR', //string -- close button text
-                    customCloseBtnClass: '', //string -- custom class for close button
-                    customConfirmBtnText: 'SI, DESINSCRIBIRME', //string -- confirm button text
-                    customConfirmBtnClass: '', //string -- custom class for confirm button
-                    onConfirm: function() {
-                        axios.delete('/ajax/usuario/inscripciones/' + idActividad).then(response => {
-                            self.$parent.traer_inscripciones()
-                            self.$parent.borro = true;
-                            setTimeout(function(){
-                                self.$parent.borro = false;
-                            }, 3000)
-                        })
-                    }
-                })
-            }
         },
         computed: {
             actividadPasada: function () {
                 let fechaFin = new Date(this.rowData.fechaFin.replace( /(\d{2})-(\d{2})-(\d{4})/, "$2/$1/$3")).getTime();
 
-                if (fechaFin  < Date.now()) {
-                    return true;
-                }
-                return false;
+                return (fechaFin < Date.now());
             },
             periodoDeEvaluacionYaComenzo: function () {
                 let fechaInicioEvaluaciones = new Date(
@@ -76,12 +52,21 @@
 
                 return (ahora > fechaInicioEvaluaciones)
             }
+        },
+        watch: {
+            // Para Mantener sincronizado el valor en casos en que el datatable filtra y el prop cambia
+            'rowData.idActividad': function (nuevoValor) {
+                this.idActividad = nuevoValor;
+            }
         }
     }
 </script>
 
 <style scoped>
- .small-text {
+ .small-caps {
      font-size: smaller;
  }
+    .contenedor {
+        margin-top: 30px
+    }
 </style>
