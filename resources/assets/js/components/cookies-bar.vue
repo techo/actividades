23ea0ec09590be32213c48d9e0b7c3fd7be02333
Alejandro Abraham <alejandro.abe88@gmail.com>
2018-08-17 10:26:03 -0300

    TEC-378. Implementación de banner de politicas de cookies

---
+++ resources/assets/js/components/cookies-bar.vue
@@ -0,0 +1,63 @@
+<template>
+    <div id="cookies-bar">
+        Este sitio utiliza cookies para brindar una correcta experiencia al usuario. Si usas este sitio, estás aceptando las
+        <a href="#">Politicas de Cookies</a>
+        <span class="pull-right" @click="cerrar">
+           <i class="fas fa-times"></i>
+       </span>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        name: "cookies-bar",
+        methods: {
+            cerrar: function () {
+                let url = "/cookie/close";
+                this.axiosGet(
+                    url,
+                    function (data, self) {
+                        $('#cookies-bar').slideUp("slow");
+                    }
+                );
+            },
+            axiosGet(url, fCallback, params = [], fError = function(){}) {
+                axios.get(url, params)
+                    .then(response => {
+                        fCallback(response.data, this)
+                    })
+                    .catch((error) => {
+                        // Error
+                        fError(error, this); //handler personalizado
+                        console.error('Error en: ' + url);
+                        if (error.response) {
+                            console.error(error.response.data);
+                            console.error(error.response.status);
+                            console.error(error.response.headers);
+                        } else if (error.request) {
+                            console.error(error.request);
+                        } else {
+                            console.error('Error', error.message);
+                        }
+                        console.error(error.config);
+                    });
+
+            },
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: black;
+        color: #eeeeee;
+        padding: 10px 5px;
+    }
+
+    span {
+        padding-right: 2%;
+        cursor: pointer;
+    }
+</style>
