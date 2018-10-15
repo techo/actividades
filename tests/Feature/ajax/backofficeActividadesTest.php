a9869619c4c45743e3fa776811df5cefbd52648a
Alejandro Abraham <alejandro.abe88@gmail.com>
2018-10-15 13:29:43 -0300

    Corrige test listar todas las actividades en backoffice

--- tests/Feature/ajax/backofficeActividadesTest.php
+++ tests/Feature/ajax/backofficeActividadesTest.php
@@ -2,9 +2,12 @@
 
 namespace Tests\Feature\ajax;
 
+use App\Persona;
+use Carbon\Carbon;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Faker\Factory;
 
 class backofficeActividadesTest extends TestCase
 {
@@ -15,7 +18,39 @@ class backofficeActividadesTest extends TestCase
      */
     public function test_listar_todas_las_actividades()
     {
-        $response = $this->get('/admin/ajax/actividades?sort=nombreActividad|asc');
+        $faker = Factory::create();
+
+        $request = new \Illuminate\Http\Request();
+        $request->setMethod('POST');
+        $request->request->add([
+            'email' => $faker->unique()->safeEmail ,
+            'nombre' => $faker->firstName,
+            'apellido' => $faker->lastName,
+            'dni' => '11111111',
+            'pais' => \App\Pais::where('nombre', 'argentina')->first(),
+            'rol' => ['rol' => 'admin'],
+            'nacimiento' => Carbon::createFromFormat('d-m-Y', '01-01-1971'),
+            'telefono' => '1',
+            'sexo' => ['id' => 'M']
+        ]);
+
+        $userService = new \App\Http\Services\UserService();
+
+        $validator = $userService->createValidator($request);
+
+        if($validator->passes()){
+            if($usuario = $userService->crearUsuario($request)){
+                $usuario->password = $userService->setPassword('password');
+                $usuario->save();
+            }
+        }
+
+        $response = $this->post('/login', [
+            'email' => $usuario->mail,
+            'password' => 'password'
+        ]);
+
+        $response = $this->actingAs($usuario)->get('/admin/ajax/actividades?sort=nombreActividad|asc');
         $response
             ->assertStatus(200)
             ->assertJsonStructure(
