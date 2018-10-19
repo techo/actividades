c547cac305b7ca49c73c6d9731c49675a24810dc
Marcos Wolff <wmarcos@gmail.com>
2018-10-19 19:59:48 -0300

    Nueva consulta para importar evaluaciones

--- database/queries/query_migracion_evaluaciones.sql
+++ database/queries/query_migracion_evaluaciones.sql
@@ -1,66 +1,77 @@
-use sigp;
+USE sigp;
 
-select puntaje_social.idEvaluador, puntaje_social.idEvaluado, puntaje_social.idActividad, puntaje_social.puntajeSocial, puntaje_constructivo.puntajeConstructivo
-    FROM
-(SELECT e.IdEncuestaRespuesta, e.idEvaluador AS idEvaluador, e.idEvaluado as idEvaluado, e.idActividad as idActividad, r.Respuesta, trim(left(o.Texto, 2))+0 as puntajeSocial, p.Texto
+CREATE TABLE IF NOT EXISTS evaluaciones_social AS (
+SELECT e.IdEncuestaRespuesta, e.idEvaluador AS idEvaluador, e.idEvaluado as idEvaluado, e.idActividad as idActividad, trim(left(o.Texto, 2))+0 as puntaje, o.Texto
 FROM Actividad act
        JOIN _EncuestaRespuestaActividad AS e ON act.idActividad = e.idActividad
        JOIN _PreguntaRespuesta AS r ON r.idEncuestaRespuesta = e.IdEncuestaRespuesta
-       JOIN _Opcion AS o ON o.idPregunta = r.IdPregunta
-                              AND o.posicion = r.Respuesta
+       JOIN _Opcion AS o ON o.idPregunta = r.IdPregunta AND o.posicion = r.Respuesta
        JOIN _Pregunta AS p ON p.IdPregunta = r.IdPregunta
-WHERE act.idActividad = 15916
-  AND idFormulario = 111
-  and e.idEvaluado = 154511
-  AND trim(left(o.Texto, 2)) REGEXP '^[0-9]+$'
-  AND p.Texto in ('Puntaje Social')) as puntaje_social
-LEFT JOIN
-(SELECT e.IdEncuestaRespuesta, e.idEvaluador as idEvaluador, e.idEvaluado as idEvaluado, e.idActividad as idActividad, r.Respuesta, trim(left(o.Texto, 2))+0 as puntajeConstructivo
+WHERE
+ idFormulario = 111 AND
+ p.Texto = 'Puntaje Social');
+
+CREATE TABLE IF NOT EXISTS evaluaciones_tecnica AS (
+SELECT e.IdEncuestaRespuesta, e.idEvaluador AS idEvaluador, e.idEvaluado as idEvaluado, e.idActividad as idActividad, trim(left(o.Texto, 2))+0 as puntaje, o.Texto
 FROM Actividad act
        JOIN _EncuestaRespuestaActividad AS e ON act.idActividad = e.idActividad
        JOIN _PreguntaRespuesta AS r ON r.idEncuestaRespuesta = e.IdEncuestaRespuesta
-       JOIN _Opcion AS o ON o.idPregunta = r.IdPregunta
-                              AND o.posicion = r.Respuesta
+       JOIN _Opcion AS o ON o.idPregunta = r.IdPregunta AND o.posicion = r.Respuesta
        JOIN _Pregunta AS p ON p.IdPregunta = r.IdPregunta
-WHERE act.idActividad = 15916
-  AND idFormulario = 111
-  and e.idEvaluado = 154511
-  AND trim(left(o.Texto, 2)) REGEXP '^[0-9]+$'
-  AND p.Texto in ('Puntaje Constructivo:')) as puntaje_constructivo
-ON puntaje_social.idEvaluador = puntaje_constructivo.idEvaluador
-AND puntaje_social.idEvaluado = puntaje_constructivo.idEvaluado
-AND puntaje_social.idActividad = puntaje_constructivo.idActividad
-AND puntaje_social.idActividad = 15916
-UNION
-select puntaje_social.idEvaluador, puntaje_social.idEvaluado, puntaje_social.idActividad, puntaje_social.puntajeSocial, puntaje_constructivo.puntajeConstructivo
+WHERE
+idFormulario = 111 AND
+p.Texto = 'Puntaje Constructivo:');
+
+CREATE INDEX i_evaluaciones_social ON evaluaciones_social (idEvaluador,idEvaluado,idActividad);
+CREATE INDEX i_evaluaciones_tecnica ON evaluaciones_tecnica (idEvaluador,idEvaluado,idActividad);
+
+-- tabla haciendo el full join
+CREATE TABLE evaluaciones AS
+   SELECT *
+   FROM (
+		SELECT
+			s.idActividad,
+			s.idEvaluado,
+			s.idEvaluador,
+			t.puntaje as tecnico,
+			s.puntaje as social,
+			s.texto
+		FROM
+			evaluaciones_social s LEFT JOIN evaluaciones_tecnica t
+			ON s.idActividad = t.idActividad
+				AND s.idEvaluado = t.idEvaluado
+				AND s.idEvaluador = t.idEvaluador
+			 JOIN Persona p ON s.idEvaluado=p.idPersona
+
+		UNION
+
+		SELECT
+			s.idActividad,
+			s.idEvaluado,
+			s.idEvaluador,
+			t.puntaje as tecnico,
+			s.puntaje as social,
+			s.texto
+		FROM
+			evaluaciones_social s RIGHT JOIN evaluaciones_tecnica t
+			ON s.idActividad = t.idActividad
+				AND s.idEvaluado = t.idEvaluado
+				AND s.idEvaluador = t.idEvaluador
+			 JOIN Persona p ON s.idEvaluado=p.idPersona
+		) ev;
+
+-- importar a tabla correspondiente
+INSERT INTO EvaluacionPersona (idActividad,idEvaluado,idEvaluador, puntajeTecnico, puntajeSocial)
+SELECT
+    idActividad,
+    idEvaluado,
+    idEvaluador,
+    tecnico,
+	social
 FROM
-     (SELECT e.IdEncuestaRespuesta, e.idEvaluador AS idEvaluador, e.idEvaluado as idEvaluado, e.idActividad as idActividad, r.Respuesta, trim(left(o.Texto, 2))+0 as puntajeSocial, p.Texto
-      FROM Actividad act
-             JOIN _EncuestaRespuestaActividad AS e ON act.idActividad = e.idActividad
-             JOIN _PreguntaRespuesta AS r ON r.idEncuestaRespuesta = e.IdEncuestaRespuesta
-             JOIN _Opcion AS o ON o.idPregunta = r.IdPregunta
-                                    AND o.posicion = r.Respuesta
-             JOIN _Pregunta AS p ON p.IdPregunta = r.IdPregunta
-      WHERE act.idActividad = 15916
-        AND idFormulario = 111
-        and e.idEvaluado = 154511
-        AND trim(left(o.Texto, 2)) REGEXP '^[0-9]+$'
-        AND p.Texto in ('Puntaje Social')) as puntaje_social
-       RIGHT JOIN
-         (SELECT e.IdEncuestaRespuesta, e.idEvaluador as idEvaluador, e.idEvaluado as idEvaluado, e.idActividad as idActividad, r.Respuesta, trim(left(o.Texto, 2))+0 as puntajeConstructivo
-          FROM Actividad act
-                 JOIN _EncuestaRespuestaActividad AS e ON act.idActividad = e.idActividad
-                 JOIN _PreguntaRespuesta AS r ON r.idEncuestaRespuesta = e.IdEncuestaRespuesta
-                 JOIN _Opcion AS o ON o.idPregunta = r.IdPregunta
-                                        AND o.posicion = r.Respuesta
-                 JOIN _Pregunta AS p ON p.IdPregunta = r.IdPregunta
-          WHERE act.idActividad = 15916
-            AND idFormulario = 111
-            and e.idEvaluado = 154511
-            AND trim(left(o.Texto, 2)) REGEXP '^[0-9]+$'
-            AND p.Texto in ('Puntaje Constructivo:')) as puntaje_constructivo
-         ON puntaje_social.idEvaluador = puntaje_constructivo.idEvaluador
-              AND puntaje_social.idEvaluado = puntaje_constructivo.idEvaluado
-              AND puntaje_social.idActividad = puntaje_constructivo.idActividad
-              AND puntaje_social.idActividad = 15916
-              AND puntaje_social.idEvaluado = 154511;
+    evaluaciones;
+
+-- borrar temporales    
+DROP TABLE evaluaciones;
+DROP TABLE evaluaciones_social;
+DROP TABLE evaluaciones_tecnica;
