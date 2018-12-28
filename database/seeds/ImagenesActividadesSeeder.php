7dcf03ce6a2b10032580549d7762b45b17870c6d
Marcos Wolff <wmarcos@gmail.com>
2018-12-28 12:34:18 -0300

    Arregla códigos de color que no estaban del todo bien

--- database/seeds/ImagenesActividadesSeeder.php
+++ database/seeds/ImagenesActividadesSeeder.php
@@ -16,19 +16,19 @@ class ImagenesActividadesSeeder extends Seeder
             ->where('id', 1)
             ->update(['imagen' => '/img/Actividades-en-asentamiento.jpg',
                 'descripcion' => 'Acompañanos y trabajemos junto a cientos de vecinos y voluntarios que luchan día a día para transformar la realidad de cientos de barrios de todo el país. No hace falta conocimientos previos, solo tu voluntad y ganas de participar.',
-              'color'=>'#448ae1']);
+              'color'=>'#0092dd']);
 
         DB::table('atl_CategoriaActividad')
             ->where('id', 2)
             ->update(['imagen' => '/img/Actividades-en-oficina.jpg',
                 'descripcion' => 'Acercate a nuestras oficinas en todo el país, conocenos y participá de actividades junto a todo el equipo. Vení a sacarte todas las dudas ¡Te esperamos!',
-              'color'=>'#d86644']);
+              'color'=>'#ea6d4f']);
 
         DB::table('atl_CategoriaActividad')
             ->where('id', 3)
             ->update(['imagen' => '/img/eventos-especiales.png',
                 'descripcion' => 'Porque a veces nos disfrazamos, o corremos, o corremos disfrazados, o participamos de otros eventos que no encajan bien en ningún lado. Eventos que son tan especiales que tuvimos que hacer una sección especialmente para ellos.',
-              'color'=>'#c78a00']);
+              'color'=>'#d78e01']);
 
         DB::table('Tipo')
             ->where('idCategoria', 1)
