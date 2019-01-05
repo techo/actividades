7326e5ad0ae8df0587bcc751502cc8734142b2f4
Marcos Wolff <wmarcos@gmail.com>
2019-01-04 23:37:02 -0300

    Me faltó la Factory Oficina

---
+++ database/factories/OficinaFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+use Faker\Generator as Faker;
+use Carbon\Carbon;
+
+$factory->define(App\Oficina::class, function (Faker $faker) {
+
+    return [
+		'nombre' => $faker->state
+	];
+
+});
