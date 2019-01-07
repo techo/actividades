e42e5515cd10ccdbf5e66de502578d0812322e15
Marcos Wolff <wmarcos@gmail.com>
2019-01-07 20:33:38 -0300

    Arregla el factory de categoría para que cargue un texto en la descripción y una foto por defecto. Arregla la vista de la home para que imprima de a tres categorías.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,43 +13,46 @@
 
 @section('main_content')
     <h1 class="text-primary font-weight-bold">¿En qué actividad quieres participar?</h1>
-    <div class="card-deck home">
+    
         @while($categoriaActividad->count() > 0)
             @php($i = 0)
-            @while ( ($i <= 3) AND ($categoriaActividad->count() > 0))
+            <div class="card-deck home">
+            @while ( ($i < 3) AND ($categoriaActividad->count() > 0))
                 @php($categoria = $categoriaActividad->shift())
-                    <div class="card border-light">
-                        <a href="/actividades?categoria={{ $categoria->id }}">
-                            <img
-                                    class="card-img-top img-fluid"
-                                    src="{{ $categoria->imagen }}"
-                                    alt="{{ $categoria->nombre }}"
-                                    width="380"
-                            >
-                        </a>
-                        <div class="card-body px-0">
-                            <h3 class="card-title">{{ $categoria->nombre }}</h3>
-                            <p class="card-text">{{ $categoria->descripcion }}</p>
-                            <p>
-                                <a class="techo-h6 techo-blue" data-toggle="collapse" href="#collapse_{{ $categoria->id }}" role="button">
-                                    Actividades a Realizar <i class="fas fa-chevron-down"></i>
+                    
+                        <div class="card border-light">
+                            <a href="/actividades?categoria={{ $categoria->id }}">
+                                <img
+                                        class="card-img-top img-fluid"
+                                        src="{{ $categoria->imagen }}"
+                                        alt="{{ $categoria->nombre }}"
+                                        width="380"
+                                >
+                            </a>
+                            <div class="card-body px-0">
+                                <h3 class="card-title">{{ $categoria->nombre }}</h3>
+                                <p class="card-text">{{ $categoria->descripcion }}</p>
+                                <p>
+                                    <a class="techo-h6 techo-blue" data-toggle="collapse" href="#collapse_{{ $categoria->id }}" role="button">
+                                        Actividades a Realizar <i class="fas fa-chevron-down"></i>
 
-                                </a>
-                            </p>
-                            <div class="collapse" id="collapse_{{ $categoria->id }}">
-                                <ul>
-                                    @foreach($categoria->tipos as $tipo)
-                                        <li>{{ $tipo->nombre }}</li>
-                                    @endforeach
-                                </ul>
+                                    </a>
+                                </p>
+                                <div class="collapse" id="collapse_{{ $categoria->id }}">
+                                    <ul>
+                                        @foreach($categoria->tipos as $tipo)
+                                            <li>{{ $tipo->nombre }}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                                <a href="/actividades?categoria={{ $categoria->id }}" class="btn btn-primary">{{ $categoria->nombre == "Eventos Especiales" ? "Ver eventos" : "Quiero saber más"}}</a>
                             </div>
-                            <a href="/actividades?categoria={{ $categoria->id }}" class="btn btn-primary">{{ $categoria->nombre == "Eventos Especiales" ? "Ver eventos" : "Quiero saber más"}}</a>
                         </div>
-                    </div>
                 @php($i++)
             @endwhile
+            </div>
         @endwhile
-    </div>
+    
 
 @endsection
 
