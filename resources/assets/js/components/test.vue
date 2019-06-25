1bdcb61e4e5400d70ffb2bfb324d3365c058ccbe
Marcos Wolff <wmarcos@gmail.com>
2019-06-25 17:01:49 -0300

    Test ejemplo para vue test utils

---
+++ resources/assets/js/components/test.vue
@@ -0,0 +1,12 @@
+<template>
+</template>
+<script>
+	export default {
+		name: 'Test',
+		data () {
+			return { test: true }
+		}
+	}
+</script>
+</style>
+</style>
---
+++ tests/Vue/example.spec.js
@@ -0,0 +1,11 @@
+import { mount } from 'vue-test-utils';
+import expect from 'expect';
+import Test from '../../resources/assets/js/components/test.vue';
+
+describe ('Test', () => {
+	it ('Testea los tests', () => {
+		let wrapper = mount(Test);
+		expect(wrapper.vm.test).toBe(true);
+	})
+
+});
