71c92a3991a8681dabd78c81781e111f77f0fab2
Marcos Wolff <wmarcos@gmail.com>
2019-06-27 18:39:57 -0300

    Test muestra errores Test reset de formulario

--- tests/Vue/inscripciones-inscribir-modal.spec.js
+++ tests/Vue/inscripciones-inscribir-modal.spec.js
@@ -9,16 +9,25 @@ import vSelect from 'vue-select'
 let Test = Vue.component('inscripciones-inscribir-modal', {
 	template: `
 		<div>
-			<v-select @search="onSearch" label="nombre" v-model="personaSeleccionada">
-			</v-select>
-			<select v-model="form.idPuntoEncuentro">
-				<option v-for="punto in puntos" :value="punto.id" >{{ punto.punto }}</option>
-			</select>
-			<button class="btn btn-primary" @click="submit" >Inscribir</button>
-			<button class="btn">Cancelar</button>
+			<div :class="{ 'form-group': true, 'has-error': errors.idPersona }" >
+				<v-select @search="onSearch" label="nombre" v-model="personaSeleccionada">
+				</v-select>
+				<span v-if="errors.idPersona" v-text="errors.idPersona[0]" class="help-block" ></span>
+			</div>
+
+			<div :class="{ 'form-group': true, 'has-error': errors.idPuntoEncuentro }" >
+				<select v-model="form.idPuntoEncuentro">
+					<option v-for="punto in puntos" :value="punto.id" >{{ punto.punto }}</option>
+				</select>
+				<span v-if="errors.idPuntoEncuentro" v-text="errors.idPuntoEncuentro[0]" class="help-block" ></span>
+			</div>
+
+			<button ref="inscribir" class="btn btn-primary" @click="submit" >Inscribir</button>
+			<button ref="cancelar" class="btn" @click="reset" >Cancelar</button>
 		</div>
 	`,
 	components: { 'v-select': vSelect },
+	props: [ 'idActividad' ],
 	data: function () {
 		return {
 			puntos: null,
@@ -27,39 +36,52 @@ let Test = Vue.component('inscripciones-inscribir-modal', {
 			form: {
 				idPersona: null,
 				idPuntoEncuentro: null,
+				//idActividad: 18029
+				//idGrupo: 266
+				//rol: ''
 			},
-			errors: null
+			errors: {}
 		}
 	},
 	mounted() {
-		axios.get('/admin/ajax/actividades/0/grupos')
-			.then((datos) => {
-				this.puntos = datos.data;
-			})
+		axios.get('/admin/ajax/actividades/' + this.idActividad + '/grupos')
+			.then((datos) => { this.puntos = datos.data; })
 			.catch((error) => { console.log(error) });
 	},
+	watch: {
+		personaSeleccionada: function (v) {
+			if(v) this.form.idPersona = v.idPersona;
+		}
+	},
 	methods: {
-		onSearch: function () {
-			axios.get('/ajax/coordinadores?coordinador=arturo')
-				.then((datos) => {
-					this.personas = datos.data;
-				})
+		onSearch: function (text, loading) {
+			axios.get('/ajax/coordinadores?coordinador=' + text)
+				.then((datos) => { this.personas = datos.data; })
 				.catch((error) => { console.log(error) });
+			loading(false);
 		},
 		submit: function () {
-			this.form.idPersona = this.personaSeleccionada.idPersona;
-
-			axios.post('/admin/ajax/actividades/18029/inscripciones')
-				.then((datos) => {
-					this.$emit('mensaje:ok')
-				})
-				.catch((error) => { console.log(error) });
+			//console.log("submit!")
+			axios.post('/admin/ajax/actividades/' + this.idActividad + '/inscripciones')
+				.then((datos) => { this.$emit("mensaje:ok"); })
+				.catch((error) => { this.errors = error.response.data.errors; });
+		},
+		reset: function () {
+	    	for (let field in this.form) {
+	    		this.form[field] = null;
+	    	}
+	    	this.reset_errors();
+		},
+		reset_errors: function () {
+			for (let field in this.errors) {
+	    		delete this.errors[field];
+	    	}
 		}
 	}
 });
 
 
-describe ('Se carga correctamente', () => {
+describe ('Se puede inscribir un usuario a una actividad', () => {
 
 	beforeEach(function () {
       // import and pass your custom axios instance to this method
@@ -72,9 +94,13 @@ describe ('Se carga correctamente', () => {
     });
 	
 	it ('Carga puntos de encuentro', (done) => {
-		let wrapper = mount(Test);
+		let wrapper = mount(Test, {
+			propsData: {
+				idActividad: 1
+			}
+		});
 
-		moxios.stubRequest('/admin/ajax/actividades/0/grupos', {
+		moxios.stubRequest('/admin/ajax/actividades/' + wrapper.props().idActividad + '/grupos', {
 	        status: 200,
 	        response: [
 	        	{"id":5513,"punto":"Palermo","pais":"Argentina","provincia":"Capital Federal","localidad":"Palermo","nombres":"Ornella","apellidoPaterno":"Morbelli"},
@@ -112,8 +138,11 @@ describe ('Se carga correctamente', () => {
 		
 	});
 
-	it ('prueba completa', (done) => {
+	it ('Elije opciones y envia', (done) => {
 		let wrapper = mount(Test, {
+			propsData: {
+				idActividad: 1
+			},
 			data: () => ({
 				personaSeleccionada: null, 
 				personas: [
@@ -127,7 +156,7 @@ describe ('Se carga correctamente', () => {
 			}),
 		});
 
-		moxios.stubRequest('POST', '/admin/ajax/actividades/18029/inscripciones', {
+		moxios.stubRequest('POST', '/admin/ajax/actividades/' + wrapper.props().idActividad + '/inscripciones', {
 	        status: 200,
 	        response: [ { mensaje: 'ok' }]
       	});
@@ -160,6 +189,109 @@ describe ('Se carga correctamente', () => {
 				done();
 	        })
 		})
+
+	})
+
+	it ('Muestra error para punto de encuentro', (done) => {
+		let wrapper = mount(Test, {
+			propsData: { idActividad: 1 },
+			data: () => ({ form: { idPersona: 141032, } }),
+		});
+
+		wrapper.find('button.btn-primary').trigger('click');
+		
+		moxios.wait(function () {
+			let request = moxios.requests.mostRecent();
+
+			request.respondWith({
+				status: 422,
+				response: { errors: { idPuntoEncuentro: ['El campo idPuntoEncuentro es requerido.'] } }
+			}).then(function () {
+				expect(wrapper.findAll('span.help-block').length).toBe(1);
+				expect(wrapper.findAll('div.has-error').length).toBe(1);
+				done();
+			});
+
+		})
+
+	})
+
+	it ('Muestra error para persona', (done) => {
+		let wrapper = mount(Test, {
+			propsData: { idActividad: 1 },
+			data: () => ({ form: { idPuntoEncuentro: 5514, } }),
+		});
+
+		wrapper.find('button.btn-primary').trigger('click');
+		
+		moxios.wait(function () {
+			let request = moxios.requests.mostRecent();
+
+			request.respondWith({
+				status: 422,
+				response: { errors: { idPersona: ['El campo idPersona es requerido.'] } }
+			}).then(function () {
+				expect(wrapper.findAll('span.help-block').length).toBe(1);
+				expect(wrapper.findAll('div.has-error').length).toBe(1);
+				done();
+			});
+
+		})
+
+	})
+
+	it ('Muestra todos los errores', (done) => {
+		let wrapper = mount(Test, {
+			propsData: { idActividad: 1 },
+			data: () => ({ form: {} }),
+		});
+
+		wrapper.find('button.btn-primary').trigger('click');
+		
+		moxios.wait(function () {
+			let request = moxios.requests.mostRecent();
+
+			request.respondWith({
+				status: 422,
+				response: { errors: { 
+					idPersona: ['El campo idPersona es requerido.'],
+					idPuntoEncuentro: ['El campo idPuntoEncuentro es requerido.']
+				} }
+			}).then(function () {
+				expect(wrapper.findAll('span.help-block').length).toBe(2);
+				expect(wrapper.findAll('div.has-error').length).toBe(2);
+				done();
+			});
+
+		})
+
+	})
+
+	it ('Botón cancelar limpia el formulario y los errores', () => {
+		let wrapper = mount(Test, {
+			propsData: { idActividad: 1 },
+			data: () => ({ 
+				form: {
+					idPuntoEncuentro: 1,
+					idPersona: 1,
+				},
+				errors: { 
+					idPersona: ['El campo idPersona es requerido.'],
+					idPuntoEncuentro: ['El campo idPuntoEncuentro es requerido.']
+				}
+			}),
+		});
+
+		//click en cancelar
+		wrapper.find({ref: 'cancelar'}).trigger('click');
+
+		//limpia formulario
+		expect(wrapper.find(vSelect).vm.value).toBe(null);
+		expect(wrapper.find('select').element.value).toBe("");
+
+		//limpia errores
+		expect(wrapper.findAll('span.help-block').length).toBe(0);
+		expect(wrapper.findAll('div.has-error').length).toBe(0);
 
 	})
 
