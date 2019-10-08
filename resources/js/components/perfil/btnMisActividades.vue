31171909c894d3d7286b68afde50b54a185da48f
agustempo <agusvilas@gmail.com>
2019-10-08 12:19:46 -0300

    488- boton evaluar entre fechas corregido y visible para inscriptos presentes

--- resources/js/components/perfil/btnMisActividades.vue
+++ resources/js/components/perfil/btnMisActividades.vue
@@ -2,7 +2,7 @@
     <div class="contenedor">
         <simplert ref="confirmar"></simplert>
         <button
-                v-if="actividadPasada && periodoDeEvaluacionYaComenzo"
+                v-if="actividadPasada && periodoDeEvaluacionYaComenzo && inscripcionPresente"
                 class="btn btn-sm btn-info"
                 @click="ir_a_evaluar"
         >
@@ -35,13 +35,14 @@
         },
         computed: {
             actividadPasada: function () {
-                let aver = moment().isAfter(moment(this.rowData.fechaFin, "DD/MM/YYYY mm:ss"));
-                //debugger;
-                return moment().isAfter(moment(this.rowData.fechaFin, "DD/MM/YYYY mm:ss"));
+                return moment().isAfter(moment(this.rowData.fechaFin, "DD/MM/YYYY hh:mm:ss"));
             },
             periodoDeEvaluacionYaComenzo: function () {
-                return moment().isAfter(moment(this.rowData.fechaInicioEvaluaciones, "DD/MM/YYYY mm:ss")) &&
-                    moment().isBefore(moment(this.rowData.fechaFinEvaluaciones, "DD/MM/YYYY mm:ss"))
+                return moment().isSameOrAfter(moment(this.rowData.fechaInicioEvaluaciones, "DD/MM/YYYY hh:mm:ss")) &&
+                    moment().isSameOrBefore(moment(this.rowData.fechaFinEvaluaciones, "DD/MM/YYYY hh:mm:ss"))
+            },
+            inscripcionPresente: function () {
+                return this.rowData.presente;
             }
         },
         watch: {
