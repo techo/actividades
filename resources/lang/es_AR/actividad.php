8fc74167aa9dc19266ccfa7e6d3a57dae0b22332
agustempo <agusvilas@gmail.com>
2019-10-29 13:43:16 -0300

    traducciones para argentina, ademas actividades y puntos de encuentro

---
+++ resources/lang/es_AR/actividad.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Actividades Language Lines
+	|--------------------------------------------------------------------------
+	|
+	|
+	*/
+
+    'nombre' => 'Nombre de Actividad',
+    'coordinador' => 'Coordinador',
+    'estado' => 'Estado de la Actividad',
+    'categoria' => 'Categoría',
+    'tipo' => 'Tipo de Actividad',
+    'oficina' => 'Oficina',
+    'fechaInicioFinInscripcion' => 'Fecha de Inicio y Fin Inscripción',
+    'fechaInicioFinActividad' => 'Fecha de Inicio y Fin Actividad',
+    'fechaInicioFinEvaluacion' => 'Fecha de Inicio y Fin Evaluaciones',
+    'descipcion' => 'Descripción',
+    'pais' => 'País',
+    'provincia' => 'Provincia',
+    'localidad' => 'Localidad (opcional)',
+    'confirmacionManual' => 'Confirmación manual',
+    'confirmacionPago' => 'Confirmación por pago',
+    'mensajeConfirmacion' => 'Mensaje de confirmación',
+    'limiteInscriptos' => 'Límite de voluntarios',
+    'estadoInscripciones' => 'Estado de las inscripciones',
+    
+];
