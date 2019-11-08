8e6280556dc954a33b9512ca7a39f12f6dcc7cad
agustempo <agusvilas@gmail.com>
2019-11-08 08:51:50 -0300

    traduccion mail recuperar contraseña

---
+++ resources/lang/en/emails.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Email Template Lines
+	|--------------------------------------------------------------------------
+	|
+	|
+	*/
+
+	// Restablecer contraseña forgotpassowrd
+	'forgot_password_1' => "We get a request for changing your password <br> Click on the next link to continue:",
+	'forgot_password_2' => "If you haven't requested for this, just relax, we aren't going to change your password.",
+	'forgot_password_link' => 'Reset my Password',
+
+
+
+];
---
+++ resources/lang/es_AR/emails.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Email Template Lines
+	|--------------------------------------------------------------------------
+	|
+	|
+	*/
+
+	// Restablecer contraseña forgotpassword
+	'forgot_password_1' => "Recibimos un pedido para restablecer tu contraseña. <br> Sólo tienes que hacer clic en el siguiente enlace para escoger una nueva:",
+	'forgot_password_2' => 'Si no solicitaste cambiar tu contraseña, puedes hacer caso omiso de este mensaje, no cambiaremos tu contraseña actual.',
+	'forgot_password_link' => 'Establecer nueva contraseña',
+
+	
+
+
+];
