2af373c3d29c1b8fa013bfe675c4997d0aaea05f
Marcos Wolff <wmarcos@gmail.com>
2019-11-12 11:37:42 -0300

    Métodos de búsqueda (actividad, provincias, tipos) filtran por variable país en la config

--- tests/Feature/ActividadesTest.php
+++ tests/Feature/ActividadesTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\ActividadFactory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Config;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 use Tests\TestCase;
@@ -12,6 +13,92 @@ use Tests\TestCase;
 class ActividadesTest extends TestCase
 {
 	use RefreshDatabase;
+
+    /** @test */
+    public function usuario_puede_ver_actividades_de_un_pais()
+    {
+        $this->withoutExceptionHandling();
+        $this->seed('PermisosSeeder');
+
+        $francia = factory('App\Pais')->create();
+        $argentina = factory('App\Pais')->create();
+
+        $actividad = app(ActividadFactory::class)
+            ->agregarPuntoConInscriptos(0)
+            ->conPais($francia->id)
+            ->create();
+
+        $actividad_2 = app(ActividadFactory::class)
+            ->agregarPuntoConInscriptos(0)
+            ->conPais($argentina->id)
+            ->create();
+
+        Config::set('app.pais', $francia->id);
+        $this->get('/ajax/actividades')->assertJsonCount(1, 'data');
+
+        Config::set('app.pais', $argentina->id);
+        $this->get('/ajax/actividades')->assertJsonCount(1, 'data');
+    }
+
+    /** @test */
+    public function usuario_puede_ver_provincias_y_localidades_segun_pais()
+    {
+        $this->withoutExceptionHandling();
+        $this->seed('PermisosSeeder');
+
+        $francia = factory('App\Pais')->create();
+        $argentina = factory('App\Pais')->create();
+
+        $actividad = app(ActividadFactory::class)
+            ->agregarPuntoConInscriptos(0)
+            ->conPais($francia)
+            ->create();
+
+        $actividad_2 = app(ActividadFactory::class)
+            ->agregarPuntoConInscriptos(0)
+            ->conPais($argentina)
+            ->create();
+
+        Config::set('app.pais', $francia->id);
+        $this
+            ->get('/ajax/actividades/provincias')
+            ->assertJsonCount(1);
+
+        Config::set('app.pais', $argentina->id);
+        $this
+            ->get('/ajax/actividades/provincias')
+            ->assertJsonCount(1);
+    }
+
+    /** @test */
+    public function usuario_puede_ver_tipos_segun_pais()
+    {
+        $this->withoutExceptionHandling();
+        $this->seed('PermisosSeeder');
+
+        $francia = factory('App\Pais')->create();
+        $argentina = factory('App\Pais')->create();
+
+        $actividad = app(ActividadFactory::class)
+            ->agregarPuntoConInscriptos(0)
+            ->conPais($francia)
+            ->create();
+
+        $actividad_2 = app(ActividadFactory::class)
+            ->agregarPuntoConInscriptos(0)
+            ->conPais($argentina)
+            ->create();
+
+        Config::set('app.pais', $francia->id);
+        $this
+            ->get('/ajax/actividades/tipos')
+            ->assertJsonCount(1);
+
+        Config::set('app.pais', $argentina->id);
+        $this
+            ->get('/ajax/actividades/tipos')
+            ->assertJsonCount(1);
+    }
 
 	/** @test */
     public function usuario_puede_crear_actividad()
