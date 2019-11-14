206a1436d8ddc9ae5e373e40ba0f50ff4e9f8b19
agustempo <agusvilas@gmail.com>
2019-11-14 05:40:37 -0300

    traduccion mails al ingles

---
+++ resources/lang/en/email.php
@@ -0,0 +1,95 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Email Template Lines
+	|--------------------------------------------------------------------------
+	|
+	|
+	*/
+
+	// generales
+	'begins_on' => 'that begins on',
+	'begins_at' => 'at',
+	'greetings' => 'See you soon!',
+	'has_been' => 'has been',
+	'cancelada' => 'CANCELED', 
+	'coordinator_message' => 'Coordinator Message:',
+	'here' => 'Here',
+	'from' => 'from',
+	'unsuscribe' => "I don't want to receive more mails",
+	'email_verification' => 'VERIFY EMAIL',
+	'profile' => 'profile',
+	'hour' => 'When?',
+
+
+
+	// actualizar Actividad
+	'activity_update_1' => 'There has been changed on the activity you have apply. Here is the updated Activity information:',
+
+	// actividad cancelada
+	'activity_canceled_1' => 'We inform you that the activity ',
+	'activity_canceled_2' => 'We are sorry about any inconvenient caused by this and we encourage you to search another activity in our page.',
+
+// Restablecer contraseña forgotpassword
+	'forgot_password_1' => "We received a request to change your password, click in the next link to choose a new one:",
+	'forgot_password_2' => "If you had not ask for this, dismiss this mail, we won't change it.",
+	'forgot_password_link' => 'Reset my Password',
+
+
+	// Inscripcion Cofirmada
+	'inscription_confirmed_1' => "You're already confirmed to participate at ",
+
+	// InscripcionEsperarConfirmacion
+	'pre_enroll_1' => 'You are pre enrolled to: ',
+	'pre_enroll_2' => 'We are going to confirm you soon!',
+	'pre_enroll_3' => 'Wait for our confirmation and in case you are in doubt you can contact de coordinator of the activity.',
+
+	// InscripcionFaltaPago
+	'missing_payment_1' => 'You are pre enrolled to: ',
+	'missing_payment_2' => 'CONFIRM BY DONATION',
+	'missing_payment_3' => 'You have time untill the ',
+	'missing_payment_4' => 'To confirm now, click on the next link ',
+	'confirm_by_donation' => 'DONATE NOW',
+	'missing_payment_5' => 'The minimun ammount is ',
+	'missing_payment_6' => 'this donation will be used to pay insurance for you, transportation and food during the activity.',
+	'missing_payment_7' => "In case you can't donate, don't worry ",
+	'missing_payment_8' => 'ask the cordinator for a grant ',
+
+	// inscripcionPagoFueraDeFecha
+	'payment_outdated_1' => "We received your donation but it's you outdated!",
+	'payment_outdated_2' => "That's why you're not confirmed in the activity.",
+	'payment_outdated_3' => "Ask the cordinator to have your money back!",
+
+	// InvitacionEvaluacion
+	'evaluation_1' => 'We want to know hoy you felt at ',
+	'evaluation_2' => 'Give us your feedback on the next link ',
+	'evaluation_3' => "If you can't use the link, copy and paste on your browser the next url:",
+
+
+	// notificacionRegistracionUsuario
+	'account_registration_title' => 'Welcome abroad our activities Platform!',
+	'account_registration_1' => "You're already registered in our platform, now we need you to validate your email in the next link:",
+	'account_registration_2' => 'Once you validate your account you can access the platform to know and apply to our activities here ',
+	'account_registration_3' => 'At this platform you can:',
+	'account_registration_4' => 'Apply to our activities',
+	'account_registration_5' => 'Keep track of all your past and future activities',
+	'account_registration_6' => 'See your enrol status at each activity',
+	'account_registration_7' => 'Evaluate the activity and your team',
+	'account_registration_8' => 'Also, you can edit and update your profile from ',
+	'account_registration_9' => 'and keep track of ',
+	'account_registration_10' => 'your activities',
+
+
+	// recordatorioActividad
+	'activity_reminder_1' => 'You are enrrolled to ',
+
+	// template 
+	'template_1' => "For TECHO it's very important that you stay informed about nes activities. You can always do that at our site ',
+
+	//VerificarMail
+	'verificar_mail_1' => "We have send this to verify your account: ",
+
+];
