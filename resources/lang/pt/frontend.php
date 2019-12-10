a03c460b03ce6ea147074a490bd4104498cf8ca2
agustempo <agusvilas@gmail.com>
2019-12-10 09:57:26 -0300

    traduccion al portugues del frontend

---
+++ resources/lang/pt/frontend.php
@@ -0,0 +1,236 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Frontend Language Lines
+	|--------------------------------------------------------------------------
+	|
+	|
+	*/
+
+	// login.vue
+    'login' => 'Acessar',
+    'login_to_account' => 'Acessar à minha conta',
+    'create_account' => 'Quero ser voluntário!',
+    'forget_password' => 'Esqueci minha senha',
+    'close' => 'Fechar',
+    'forget_password' => 'Esqueci minha senha',
+    'login_facebook' => 'Acesse com Facebook',
+    'login_google' => 'Acesse com Google',
+    'mail' => 'Mail',
+	'mail_placeholder' => 'Digite seu e-mail',
+	'password' => 'Senha',
+	'password_placeholder' => 'Digite sua senha',
+	'not_a_volunteer' => 'AINDA NÃO É VOLUNTÁRIO DO TETO?',
+	'volunteer_me' => 'Quero ser voluntário!',
+	'hello' => 'Olá',
+	'activities' => 'Atividades',
+	'my_activities' => 'Minhas Actividades',
+	'profile' => 'Perfil',
+	'admin' => 'Admin',
+	'help' => 'Ajuda',
+	'logout' => 'Sair',
+	'login_null' => 'O e-mail e a senha são obrigatórios',
+
+
+	//loginController
+	'login_error' => 'E-mail e/ou senha es incorretos',
+
+
+	// reset.blade
+	'reset_password' => 'Redefinir senha',
+
+	//email.blade
+	'send_link' => 'Enviar link',
+
+
+	// home
+	'title' => 'Vamos transformar essa realidade',
+	'which_type_of_activity' => 'Em que atividade deseja participar?',
+	'activities_types' => 'Tipos de Atividades',
+	'view_activities'  =>  'Me inscrever',
+	'welcome' => 'Bemvindo',
+
+		// registro.vue
+	'register' => 'Registre-se',
+	'finish' => 'Terminar',
+	'step_1' => 'PASSO 1/3',
+	'step_2' => 'PASSO 2/3',
+	'step_3' => 'PASSO 3/3',
+    'register_facebook' => 'Registre-se com Facebook',
+    'register_google' => 'Registre-se com Google',
+    'create_password' => 'Criar nova Senha',
+    'almost_there' => 'Já quase terminamos!',
+    'i_accept_the' => 'Aceito à ' ,
+    'privacy_policy' => 'Política de Privacidade',
+    'error_privacy_policy' => 'Precisa aceitar as políticas de privacidade para continuar',
+	'already_register' => 'Você já está registrado/a como voluntário/a, esperamos você!! ;)',
+	'search_activities' => 'BUSCAR ATIVIDADES',
+	'link_to_rrss' => 'Confimar Link Rede Social',
+	'link_rrss_techo' => 'Vincule sua conta de teto con sua conta de red social',
+	'confirm' => 'Confirmar',	
+
+	// verify.blade.php
+	'confirm_your_email' => 'Verificar endereço de email',
+    'verify_email_message_1' => 'Um link de verificação foi enviado para sua caixa de e-mail',
+    'verify_email_message_2' => 'Para continuar, verifique sua caixa de e-mail e clique no link de verificação que enviamos a você.',
+    'verify_email_message_3' => 'Se você não recebeu o correo', 
+	'verify_email_resend' => 'clique para solicitar outro',
+	'verify_email' => 'Verificar email',
+
+
+
+	// perfil/perfil.vue
+	'changes_success'  =>  'As mudanças foram salvas com sucesso.',
+	'profile_text_1'  =>  'Aqui você pode fazer alterações no seu perfil. Modifique sua senha y dados pessoais.',
+    'profile_text_2'  =>  'Também pode',
+    'profile_text_3'  =>  'mudar seu endereço de e-mail',
+    'personal_data'  =>  'Dados pessoais',
+    'name'  =>  'NOME*',
+    'surname'  =>  'SOBRENOME*',
+    'born_date'  =>  'ANIVERSÁRIO*',
+    'gender'  =>  'Sexo*',
+    'gender_m'  =>  'Masculino',
+    'gender_x'  =>  'Outro',
+    'gender_o'  =>  'Prefiro não dizer',
+    'passport'  =>  'CPF / PASAPORTE*',
+    'country'  =>  'PAÍS*',
+	'state'  =>  'PROVÍNCIA',
+	'city'  =>  'LOCALIDADE',
+	'telephone'  =>  'TELEFONE*',
+	'actual_password'  =>  'SENHA ATUAL*',
+	'new_password'  =>  'NOVA SENHA*',
+    'confirm_new_password'  =>  'CONFIRMAR SENHA*',
+    'platform_notifications_agreement'  =>  'Receba notificações operacionais da plataforma (necessário para estar informado das atividades nas quais você participa)',
+    'techo_notifications_agreement'  =>  'Aceito que o TETO entre em contato para me notificar sobre eventos e campanhas',
+	'save'  =>  'Salvar', 
+    'delete_account'  =>  'Excluir minha conta',
+ 	'account_rrss_text_1' => 'Sua conta está vinculada com uma rede social, para mudar a senha precisas',
+ 	'account_rrss_text_2' => 'e clicar em',
+ 	'delete_account_message' => "Você está prestes a excluir sua conta desta plataforma. Essa ação não pode ser desfeita. Deseja continuar?",
+ 	'delete_account_confirm_button' =>'SIM, EXCLUIR',
+
+ 	'date_placeholder' => 'Selecione uma data',	
+
+
+ 	// perfil/index.blade.php
+ 	'profile' => 'Perfil',
+
+ 	// perfil/cambiar-email.blade.php
+ 	'change_email' => 'Alterar E-mail',
+ 	'change_email_title' => 'Aqui você pode fazer alterações na sua caixa de e-mail.',
+ 	'change_email_title_2' => 'Tem em conta que:',
+	'change_email_req_1' => 'É necessário verificar o novo endereço de e-mail',
+	'change_email_req_2' => 'Associações a redes sociais vão se perder.',
+	'change_email_req_3' => 'Você precisa fazer login novamente.',
+	'change_email_new_email' => 'Novo e-mail',
+	'change_email_new_email_confirmation' => 'Digite novamente seu e-mail para confirmar',
+	'change_email_are_you_sure' => 'Tem certeza?',
+	'change_email_confirm_button' => 'Sim, mudar',
+
+
+	// perfil/actividades
+	'my_activities' => 'Minhas Atividades',
+	'next_activities' => 'Próximas Atividades', 
+    'past_activities' => 'Atividades passadas',
+	'filter_placeholder' => 'Nome o localidade da actividade',
+
+
+	// perfil/btnMisActividades
+	'view_evaluations' => 'Ver avaliações',
+
+
+	// perfil/actividades.vue
+	'unenroll_ok' => "Você cancelou sua inscrição com sucesso na atividade.",
+	'enrollment_empty' => "Ei, sem inscrições futuras, hora de procurar uma! :)",
+
+
+	// perfil/tarjeta.vue
+	'unenroll_ok' => "Você cancelou sua inscrição com sucesso na atividade.",
+	'enrollment_empty' => "Ei, sem inscrições futuras, hora de procurar uma! :)",
+	'unapply' => 'Cancelar minha inscrição',
+	'absent' => 'Ausente',
+	'evaluations_start_on' => 'As avaliações começam em',
+	'unenroll_title' => 'CANCELAR MINHA INSCRIÇÃO DA ATIVIDADE',
+	'message_1' => 'Você está prestes a cancelar sua inscrição da atividade',
+	'message_2' => ' seus dados de participação serão excluídos. Você pode se inscrever quando quiser. Deseja continuar?',
+	'unenroll_button' => 'SIM, CANCELAR MINHA INSCRIÇÃO',
+
+
+	// componenets/datatable/filterBar.vue
+	'filter_by' => 'Filtrar por',
+	'filter' => 'Filtrar',
+	'delete' => 'Deletar',
+
+	// components/datatable.vue
+	'empty_records' => "Não há registros para exibir",
+	'pagination_detail' => "Mostrando :of  de :total atividades",
+
+	// filtro.vue
+	'categories' => 'CATEGORIAS',
+	'select_all' => 'Marcar tudo',
+
+
+ 	// contenedorchecktipoactividad
+ 	'activity_types' => 'Tipos de Actividade',
+
+    
+	// actividades/show.blade.php
+    'description' => '',
+    'not_defined' => 'Não definido',
+    'coordinator' => 'Coordenador',
+    'meeting_points' => 'Onde nos encontramos?',
+    'referring' => 'Liderança',
+    'share' => 'COMPARTILHAR',
+
+
+    // actividadesController
+    'error' => 'ERRO',
+    'closed_inscriptions' => 'O período de inscrição está fechado',
+	'approval_needed' => 'CONFIRME SUA PARTICIPAÇÃO',
+	'confirmation_date_is_closed' => 'DATA DE CONFIRMAÇÃO EXPIRADA',
+	'waiting_for_confirmation' => 'ESPERAR CONFIRMAÇÃO',
+    'confirmed' => 'CONFIRMADO',
+	'activity_full' => 'A atividade não tem mais lugares',
+	'pre_registration' => 'PRÉ-REGISTO',
+	'apply_now' => 'ME INSCREVER',
+
+	// pagar-paso-1
+	'last_step_confirm_by_donation' => 'Só mais um passo!',
+	'you_are_pre_registered' => 'Você já está pré-registrado/a',
+	'mail_sended' => 'E também enviamos um e-mail com todas as informações da atividade! Confira!',
+	'complete_registration' => 'Mas primeiro: confirme seu lugar!',
+	'donation_ammount' => 'VALOR PARA DOAR',
+	'suggested_donation' => 'Doação sugerida: ',
+	'suggested_donation_between' => 'Doação sugerida: Entre ',
+	'and' => ' e ',
+	'also_you_can' => 'ou também pode ',
+	'ask_for_grant' => 'SOLICITAR UMA BOLSA',
+	'go_back' => 'VOLTAR',
+	'continue' => 'PRÓXIMO',
+	'cancel' => 'CANCELAR',
+	'unenroll' => 'CANCELAR MINHA INSCRIÇÃO',
+
+	//pagar_paso_2
+	'confirm_by_paying' => 'Confirme com seu pagamento',
+	'ready_for_paying' => 'Pronto para pagar pela site!',
+	'you_choose' => 'Você escolheu doar:',
+	'redirect_pay_platform' => 'Ao clicar, você será redirecionado para o site de pagamento',
+
+	//gracias.blade.php
+    'frontend.activity_detail' => 'Detalhe da Actividade',
+    'inscription_confirmed' => 'Inscrição confirmada!',
+    'already_inscripted' => 'Você já está registrado em ',
+	'mail_message' => 'Enviamos um e-mail com mais informações sobre esta atividade. Para ver as atividades nas quais você está registrado e modificá-las, acesse à ',
+
+
+	// actividades/index.blade.php
+	'index_actividades_text' =>  'Se para você o resultado é a misma coisa, você está fazendo as contas erradas',
+	'index_actividades_text_2' => 'Inscreva-se e participe',
+	'delete_filter' => 'Apagar Filtros',
+
+
+
+];
