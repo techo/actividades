6bee4ab5f0103b90438148341adfdfc6e8e7660a
agustempo <agusvilas@gmail.com>
2019-12-18 16:04:20 -0300

    cambio query para implementacion

--- database/arreglo-actividad-campos-por-defecto.sql
+++ database/arreglo-actividad-campos-por-defecto.sql
@@ -10,8 +10,14 @@ length(limiteInscripciones) > 10;
 
 ALTER TABLE `sigp`.`Actividad` 
 CHANGE COLUMN `limiteInscripciones` `limiteInscripciones` INT NOT NULL DEFAULT 0 ;
+
+ALTER TABLE `sigp`.`Actividad` 
 CHANGE COLUMN `idUnidadOrganizacional` `idUnidadOrganizacional` INT(11) NULL ;
+
+ALTER TABLE `sigp`.`Actividad` 
 CHANGE COLUMN `inscripcionInterna` `inscripcionInterna` TINYINT(1) NOT NULL DEFAULT 0 ;
+
+ALTER TABLE `sigp`.`Actividad` 
 DROP INDEX `idUnidadOrganizacional` ;
 
 ALTER TABLE `sigp`.`PuntoEncuentro` 
