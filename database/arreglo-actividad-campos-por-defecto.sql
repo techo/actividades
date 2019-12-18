66e20c0ff9245f835861cbd6a6dfb3e041203cd4
agustempo <agusvilas@gmail.com>
2019-12-18 09:36:58 -0300

    correccion sql

--- database/arreglo-actividad-campos-por-defecto.sql
+++ database/arreglo-actividad-campos-por-defecto.sql
@@ -1,18 +1,18 @@
-UPDATE Actividad
+UPDATE sigp.Actividad
 SET limiteInscripciones = 0
 WHERE
 limiteInscripciones in ('', 'no hay', '11/02');
 
-UPDATE Actividad
+UPDATE sigp.Actividad
 SET limiteInscripciones = 0
 WHERE
-leght(limiteInscripciones) > 10;
+length(limiteInscripciones) > 10;
 
-ALTER TABLE `Actividad` 
+ALTER TABLE `sigp`.`Actividad` 
 CHANGE COLUMN `limiteInscripciones` `limiteInscripciones` INT NOT NULL DEFAULT 0 ;
 CHANGE COLUMN `idUnidadOrganizacional` `idUnidadOrganizacional` INT(11) NULL ;
 CHANGE COLUMN `inscripcionInterna` `inscripcionInterna` TINYINT(1) NOT NULL DEFAULT 0 ;
 DROP INDEX `idUnidadOrganizacional` ;
 
-ALTER TABLE `PuntoEncuentro` 
+ALTER TABLE `sigp`.`PuntoEncuentro` 
 CHANGE COLUMN `idLocalidad` `idLocalidad` INT(11) NULL DEFAULT NULL ;
