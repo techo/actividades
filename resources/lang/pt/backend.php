cd34999bf2d1f9af757bf4d284c1bec35148186e
Pedro de Robles <pdrobles@espinlabs.com.ar>
2024-04-17 23:39:24 -0300

    feature(admin/persona): Se agrego la traducción en Portugues

---
+++ resources/lang/pt/backend.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Backend Language Lines
+	|--------------------------------------------------------------------------
+	|
+	|
+	*/
+
+    // Usuarios
+    'identifications' => 'RG / Passaporte',
+    'name' => 'Nome',
+    'last_name' => 'Sobrenome',
+    'birthdate' => 'Data de nascimento',
+    'gender' => 'Gênero',
+    'phone' => 'Telefone',
+    'role' => 'Função',
+    'state' => 'Estado',
+    'country' => 'País',
+    'province' => 'Província',
+    'location' => 'Localidade',
+    'contact_channel' => 'Canal de Contato',
+    'verification' => 'Verificação',
+
+    // Botones
+    'save'   => 'Salvar',
+    'cancel' => 'Cancelar',
+    'search' => 'Pesquisar',
+    'delete' => 'Apagar',
+    'fuse' => 'Fundir',
+    'edit' => 'Editar',
+    'select' => 'Selecionar',
+    'select_date' => 'Selecionar data',
+
+    // Títulos
+    'create_person' => 'Criar Pessoa',
+];
