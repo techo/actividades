9a92391f6c9268f0f4d33682d214abbcc00b4e0d
agustempo <agusvilas@gmail.com>
2024-06-20 19:18:50 -0300

    dont show on cards and show

--- resources/views/actividades/show.blade.php
+++ resources/views/actividades/show.blade.php
@@ -37,21 +37,24 @@
 			</div>
 		</div>
 		<div class="row justify-content-start">
-			<div class="col-md-2"><i class="far fa-calendar"></i> <span>{{ $actividad->fechaInicio->format('d/m/Y')}}</span></div>
-			<div class="col-md-2"><i class="far fa-clock"></i> <span>{{ $actividad->fechaInicio->format('H:i')}}hs</span></div>
-
-            <div class="col-md-8">
-                <i class="fas fa-map-marker-alt"></i>
-                <span>
-					@if (!isset($actividad->localidad) || $actividad->localidad->localidad == "No definida")
-                        {{ $actividad->provincia->provincia }}, {{ $actividad->pais->nombre }}
-                    @elseif (!isset($actividad->provincia))
-                        Sin especificar
-                    @else
-                        {{ $actividad->localidad->localidad }}, {{ $actividad->provincia->provincia }}
-                    @endif
-                </span>
-            </div>
+            @if($actividad->show_dates)
+                <div class="col-md-2"><i class="far fa-calendar"></i> <span>{{ $actividad->fechaInicio->format('d/m/Y')}}</span></div>
+                <div class="col-md-2"><i class="far fa-clock"></i> <span>{{ $actividad->fechaInicio->format('H:i')}}hs</span></div>
+            @endif
+            @if($actividad->show_location)
+                <div class="col-md-8">
+                    <i class="fas fa-map-marker-alt"></i>
+                    <span>
+                        @if (!isset($actividad->localidad) || $actividad->localidad->localidad == "No definida")
+                            {{ $actividad->provincia->provincia }}, {{ $actividad->pais->nombre }}
+                        @elseif (!isset($actividad->provincia))
+                            Sin especificar
+                        @else
+                            {{ $actividad->localidad->localidad }}, {{ $actividad->provincia->provincia }}
+                        @endif
+                    </span>
+                </div>
+            @endif
 		</div>
 		<hr>
 		<div class="row">
@@ -71,61 +74,64 @@
                 <h5>{{ __('frontend.coordinator') }}</h5>
 			</div>
 		</div>
-			<div class="row">
-				<div class="col-md-12 p-1 ">
-                    <ul style="list-style-type:none;">
-                        @foreach($actividad->coordinadores as $coordinador)
-                            <li>
-                            @if ($coordinador->persona->photo)
-                                <img class="imagen-perfil-mini" src="{{ '/'.$coordinador->persona->photo }}" alt="Foto">
-                            @else
-                                <img src="/bower_components/admin-lte/dist/img/user_avatar.png" class="imagen-perfil-mini" alt="User Image"> 
-                            @endif
-                                <span>
-                                    {{$coordinador->persona->nombres}} {{$coordinador->persona->apellidoPaterno }}
-                                </span>
-                            </li>
-                        @endforeach
-                    </ul>
-                    
-				</div>
-			</div>
-		<hr>
-		<div class="row">
-			<div class="col-md-12">
-				<h5>{{ __('frontend.meeting_points') }}</h5>
-			</div>
-		</div>
-		@foreach($actividad->puntosEncuentro as $puntoEncuentro)
-            @if($puntoEncuentro->estado)
-		      <div class="row">
-                <div class="col-md-4">
-				    {{$puntoEncuentro->punto}}
-                </div>
-                <div class="col-md-4">
-                    @php
-                        echo isset($puntoEncuentro->localidad->localidad) ? $puntoEncuentro->localidad->localidad . ', ': '';
-                        echo isset($puntoEncuentro->provincia->provincia) ? $puntoEncuentro->provincia->provincia . '': '';                    @endphp
-
-                </div>
-                <div class="col-md-4">
-                    <strong>{{ __('frontend.referring') }}:</strong>
-                        @if ($puntoEncuentro->responsable)
-                            @if ($puntoEncuentro->responsable->photo)
-                                <img class="imagen-perfil-mini" src="{{ '/'.$puntoEncuentro->responsable->photo }}" alt="Foto">
-                            @else
-                                <img src="/bower_components/admin-lte/dist/img/user_avatar.png" class="imagen-perfil-mini" alt="User Image"> 
-                            @endif
-                            {{ $puntoEncuentro->responsable->nombreCompleto }}
+        <div class="row">
+            <div class="col-md-12 p-1 ">
+                <ul style="list-style-type:none;">
+                    @foreach($actividad->coordinadores as $coordinador)
+                        <li>
+                        @if ($coordinador->persona->photo)
+                            <img class="imagen-perfil-mini" src="{{ '/'.$coordinador->persona->photo }}" alt="Foto">
                         @else
-                            {{  __('frontend.not_defined') }}
+                            <img src="/bower_components/admin-lte/dist/img/user_avatar.png" class="imagen-perfil-mini" alt="User Image"> 
                         @endif
-
-                    </ul>
+                            <span>
+                                {{$coordinador->persona->nombres}} {{$coordinador->persona->apellidoPaterno }}
+                            </span>
+                        </li>
+                    @endforeach
+                </ul>
+                
+            </div>
+        </div>
+		<hr>
+        @if ($actividad->show_location)
+            <div  class="row">
+                <div class="col-md-12">
+                    <h5>{{ __('frontend.meeting_points') }}</h5>
                 </div>
-		      </div>
-            @endif
-		@endforeach
+            </div>
+        
+            @foreach($actividad->puntosEncuentro as $puntoEncuentro)
+                @if($puntoEncuentro->estado)
+                    <div class="row">
+                        <div class="col-md-4">
+                            {{$puntoEncuentro->punto}}
+                        </div>
+                        <div class="col-md-4">
+                            @php
+                                echo isset($puntoEncuentro->localidad->localidad) ? $puntoEncuentro->localidad->localidad . ', ': '';
+                                echo isset($puntoEncuentro->provincia->provincia) ? $puntoEncuentro->provincia->provincia . '': '';                    @endphp
+
+                        </div>
+                        <div class="col-md-4">
+                            <strong>{{ __('frontend.referring') }}:</strong>
+                                @if ($puntoEncuentro->responsable)
+                                    @if ($puntoEncuentro->responsable->photo)
+                                        <img class="imagen-perfil-mini" src="{{ '/'.$puntoEncuentro->responsable->photo }}" alt="Foto">
+                                    @else
+                                        <img src="/bower_components/admin-lte/dist/img/user_avatar.png" class="imagen-perfil-mini" alt="User Image"> 
+                                    @endif
+                                    {{ $puntoEncuentro->responsable->nombreCompleto }}
+                                @else
+                                    {{  __('frontend.not_defined') }}
+                                @endif
+
+                            </ul>
+                        </div>
+                    </div>
+                @endif
+            @endforeach
+        @endif
         </div>
     </div>
 
