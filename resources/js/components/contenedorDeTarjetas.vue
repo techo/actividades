85b44508df2b315881212c98aed218eb60ef01ba
Pedro de Robles <pdrobles@espinlabs.com.ar>
2024-09-06 07:56:42 -0300

    Refactor(Index actividades): Mostrar carouseles por categorias acorde a un array de categorias

--- resources/js/components/contenedorDeTarjetas.vue
+++ resources/js/components/contenedorDeTarjetas.vue
@@ -1,7 +1,19 @@
 <template>
     <span>
         <!-- <div class="row py-2"> -->
-            <carousel-de-tarjetas v-show="!vacio && !loading.cat1"  :actividades="actividades" :title="'Con el pie en territorio, todas las actividades en comunidad.'"/>
+            <div v-show="loading" class="loading" style="text-align: center">
+                <i class="fas fa-sync fa-spin fa-3x"></i>
+            </div>
+            <div v-show="!loading">
+                <div v-for="categoria in categorias" :key="categoria">
+                    <carousel-de-tarjetas
+                        v-show="actividadesPorCategoria[categoria] && actividadesPorCategoria[categoria].length > 0"
+                        :actividades="actividadesPorCategoria[categoria] || []"
+                        :title="'Actividades para la categoría ' + categoria"
+                    />
+                </div>
+            </div>
+            <!-- <carousel-de-tarjetas v-show="!vacio && !loading.cat1"  :actividades="actividades" :title="'Con el pie en territorio, todas las actividades en comunidad.'"/>
             <div v-show="loading.cat1" class="loading" style="text-align: center">
                 <i class="fas fa-sync fa-spin fa-3x"></i>
             </div>
@@ -9,7 +21,7 @@
             <carousel-de-tarjetas v-show="!vacio && !loading.cat2"  :actividades="actividades2" :title="'Fortalece tus capacidades Capacitaciones, Espacios formativos y Talleres'"/>
             <div v-show="loading.cat2" class="loading" style="text-align: center">
                 <i class="fas fa-sync fa-spin fa-3x"></i>
-            </div>
+            </div> -->
         <!-- </div> -->
 
         <!-- <div v-show="loading" class="loading" style="text-align: center">
@@ -32,9 +44,9 @@
 
         data () {
             return {
-                actividades: [],
-                actividades2: [],
-                loading: { cat1: false, cat2: false },
+                categorias: [1,2,3, 4],
+                actividadesPorCategoria: {},
+                loading: false,
                 next_page: '',
                 bottom: false,
                 url: '/ajax/actividades',
@@ -44,24 +56,20 @@
                 filtros: {}
             }
         },
-        components: {Suscribe, tarjeta: Tarjeta},
+        components: { Suscribe, tarjeta: Tarjeta, CarouselDeTarjetas },
+
         created () {
+            this.inicializarActividadesPorCategoria();
             window.addEventListener('scroll', () => {
-                this.bottom = this.bottomVisible()
+                this.bottom = this.bottomVisible();
             });
             window.addEventListener('cargarTarjetas', async (event) => {
-                // event.detail.categoria = 2;
                 this.filtros = event.detail;
-                this.actividades = [];
-                this.actividades2 = [];
-                
-                // this.agregarTarjetas(this.url, this.filtros, true, 1);
-                // this.agregarTarjetas(this.url, this.filtros, true, 2);
                 await this.cargarTarjetas();
             });
         },
-        methods: {
 
+        methods: {
             bottomVisible() {
                 const scrollY = window.scrollY;
                 const visible = document.documentElement.clientHeight;
@@ -69,82 +77,58 @@
                 const bottomOfPage = visible + scrollY >= pageHeight;
                 return bottomOfPage || pageHeight < visible;
             },
+            inicializarActividadesPorCategoria() {
+                // Inicializar actividadesPorCategoria según categorias
+                this.categorias.forEach(categoria => {
+                    this.$set(this.actividadesPorCategoria, categoria, []);
+                    // this.$set(this.loading, `cat${categoria}`, false);
+                });
+            },
             async agregarTarjetas(url, filtros, refresh, categoria) {
+                // Evitar concurrencia
+                // if (this.loading[`cat${categoria}`]) { return; }
 
-                //forma cabeza de evitar concurrencia
-                if (this.loading[`cat${categoria}`]) { return; };
-
-                let self = this;
-                this.loading[`cat${categoria}`] = true;
+                // this.loading[`cat${categoria}`] = true;
                 this.vacio = false;
                 filtros.categoria = categoria;
 
-                axios.get(url, { params:  filtros })
-                    .then(response => {
-                        console.log(response.data);
+                try {
+                    const response = await axios.get(url, { params: filtros });
 
-                        if(typeof response.data.data == "undefined" || response.data.data.length == 0) {
-                            this.loading[`cat${categoria}`] = false;
-                            this.vacio = true;
-                        }
-                        
-                        if (refresh) {
-                            if (categoria === 1) {
-                                this.actividades = [];
-                            } else if (categoria === 2) {
-                                this.actividades2 = [];
-                            }
-                        }
+                    // if (typeof response.data.data === "undefined" || response.data.data.length === 0) {
+                    //     // this.loading[`cat${categoria}`] = false;
+                    //     this.vacio = true;
+                    // }
 
-                        for (let element in response.data.data) {
-                            if (categoria == 1) {
-                                self.actividades.push(response.data.data[element]);
-                            }
-                            if (categoria == 2) {
-                                self.actividades2.push(response.data.data[element]);
-                            }
-                        }
-                        
-                        this.next_page = response.data.next_page_url;
-                        this.ultimaTarjeta = response.data.to;
-                        this.totalTarjetas = response.data.total;
+                    if (refresh) {
+                        this.actividadesPorCategoria[categoria] = [];
+                    }
 
-                        this.loading[`cat${categoria}`] = false;
+                    this.actividadesPorCategoria[categoria].push(...response.data.data);
 
-                    })
-                    .catch((error) => {
-                        // Error
-                        console.error('error en contenedor de tarjetas');
-                        if (error.response) {
-                            // The request was made and the server responded with a status code
-                            // that falls out of the range of 2xx
-                            console.log(error.response.data);
-                            console.log(error.response.status);
-                            console.log(error.response.headers);
-                        } else if (error.request) {
-                            // The request was made but no response was received
-                            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-                            // http.ClientRequest in node.js
-                            console.log(error.request);
-                        } else {
-                            // Something happened in setting up the request that triggered an Error
-                            console.log('Error', error.message);
-                        }
-                        console.log(error.config);
-                    });
+                    this.next_page = response.data.next_page_url;
+                    this.ultimaTarjeta = response.data.to;
+                    this.totalTarjetas = response.data.total;
+
+                    // this.checkLoadingGlobal();
+                } catch (error) {
+                    console.error('Error en contenedor de tarjetas', error);
+                }
             },
             async cargarTarjetas() {
-                // Primero cargar la categoría 2
-                await this.agregarTarjetas(this.url, this.filtros, true, 1);
-                // Luego cargar la categoría 1
-                await this.agregarTarjetas(this.url, this.filtros, true, 2);
+                this.loading = true;
+                for (let categoria of this.categorias) {
+                    await this.agregarTarjetas(this.url, this.filtros, true, categoria);
+                }
+                this.loading = false;
+                this.vacio = !Object.values(this.actividadesPorCategoria).some(actividades => actividades.length > 0);
             }
         },
 
         watch: {
             bottom(bottom) {
                 if (bottom && this.ultimaTarjeta && this.totalTarjetas) {
-                    if (this.ultimaTarjeta < this.totalTarjetas ) {
+                    if (this.ultimaTarjeta < this.totalTarjetas) {
                         this.agregarTarjetas(this.next_page, this.filtros, false);
                     }
                 }
