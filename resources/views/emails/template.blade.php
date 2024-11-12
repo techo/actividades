294465d29e6c525a418f67a3fd328469767cd08a
agustempo <agusvilas@gmail.com>
2024-11-12 12:27:05 -0300

    bootstrap on mails

--- resources/views/emails/template.blade.php
+++ resources/views/emails/template.blade.php
@@ -4,6 +4,8 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+
+    @include('partials.css')
 </head>
 <body>
 <style>
