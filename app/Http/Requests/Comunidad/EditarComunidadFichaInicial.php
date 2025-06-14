e0c1ff2e251801494bc486603b57be0f0553c9cd
agustempo <agusvilas@gmail.com>
2025-06-14 11:30:05 -0300

    permisos

--- app/Http/Requests/Comunidad/EditarComunidadFichaInicial.php
+++ app/Http/Requests/Comunidad/EditarComunidadFichaInicial.php
@@ -2,13 +2,28 @@
 
 namespace App\Http\Requests\Comunidad;
 
+use App\Comunidad;
 use Illuminate\Foundation\Http\FormRequest;
 
 class EditarComunidadFichaInicial extends FormRequest
 {
     public function authorize()
     {
-        return true; 
+        $idComunidad = array_slice(explode('/', request()->path()), array_search('comunidades', explode('/', request()->path())) + 1, 1)[0];
+        $idPaisActividad = Comunidad::findOrFail($idComunidad)->idPais;
+        $user = auth()->user();
+
+        if ($user->hasRole('admin')) {
+            return $idPaisActividad == auth()->user()->idPaisPermitido;
+        }
+
+        // Si es coordinador y pertenece a esta comunidad
+        if ($user->hasRole('coordinador')) {
+            $comunidad = Comunidad::with('coordinadores')->find($idComunidad);
+
+            return $comunidad &&
+                $comunidad->coordinadores->contains('idPersona', $user->idPersona);
+        }
     }
 
     public function rules()
